77a25686b24f8383b0a4fdebad68aea083300436
holajm <holajjm@naver.com>
2025-01-10 14:26:19 +0900

    Feat: 카카오 로그인 구현
    백엔드 서버와 카카오 서버 연동 후 카카오 로그인 구현 후 카카오 유저 정보 받아오기 성공

--- src/pages/User/UserLogin.tsx
+++ src/pages/User/UserLogin.tsx
@@ -4,13 +4,25 @@ import { useNavigate } from "react-router-dom"
 
 function UserLogin() {
   const navigate = useNavigate();
-  const handleClick = () => {
-    navigate("/join")
-  }
   const fakeLogin = () => {
-    sessionStorage.setItem("fake","fakeID");
-    alert("임시 로그인 완료");
-    navigate("/minihome");
+    sessionStorage.setItem("fake", "fakeID")
+    alert("임시 로그인 완료")
+    navigate("/minihome")
+  }
+  const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const kakaoLogin = async () => {
+    window.location.href = kakaoURL;
+  }
+
+  const githubLogin = async () => {
+    const response = await fetch(
+      "https://github.com/login/oauth/authorize?client_id=Iv23lica8LFNbzOrB595",
+    )
+    const data = await response.json()
+    console.log(data)
   }
   return (
     <div className={style.container}>
@@ -24,10 +36,10 @@ function UserLogin() {
         </header>
         <main className={style.main}>
           <div className={style.main_wrapper}>
-            <button onClick={handleClick} className={style.kakao}>
+            <button onClick={kakaoLogin} className={style.kakao}>
               <img src="/images/KakaoLogin.png" alt="KakaoLogin" />
             </button>
-            <button onClick={handleClick} className={style.github}>
+            <button onClick={githubLogin} className={style.github}>
               <img
                 src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
                 alt="GitHub Logo"
