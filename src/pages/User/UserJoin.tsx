d8910a20065d13ea2ec6c98a4e9e4742af7bbb5a
holajm <holajjm@naver.com>
2025-01-13 17:51:22 +0900

    Feat: 유저 회원가입 로직 구현

--- src/pages/User/UserJoin.tsx
+++ src/pages/User/UserJoin.tsx
@@ -1,7 +1,51 @@
-import React from "react"
-import style from "@styles/User/UserJoin.module.css"
+import React from "react";
+import style from "@styles/User/UserJoin.module.css";
+import { useForm } from "react-hook-form";
+import { useUserStore } from "@store/store";
+import { useNavigate } from "react-router-dom";
 
+interface Data {
+  image: string;
+  nickname: string;
+}
 function UserJoin() {
+  const {user,setUser} = useUserStore((state) => state);
+  const navigate = useNavigate();
+  console.log(user);
+  
+  const defaultImage = "/images/Camera.svg";
+
+  const {register, handleSubmit, formState: {errors}} = useForm<Data>();
+
+  const onSubmit = async () => {
+    const response = await fetch("http://61.79.183.245:80/join",{
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "nickname": user.nickname,
+        "loginType": user.loginType,
+        "loginId": user.loginId,
+        "profileUrl": user.profileUrl
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+
+    if(data){
+      setUser({
+        ...user,
+        accessToken: data.accessToken,
+        refreshToken: data.refreshToken,
+      })
+      localStorage.setItem("AccessToken", JSON.stringify(data.accessToken));
+      localStorage.setItem("RefreshToken", JSON.stringify(data.refreshToken));
+      alert("회원가입 완료");
+      navigate("/main");
+    }
+  }
+  
   return (
     <div className={style.container}>
       <section className={style.wrapper}>
@@ -12,28 +56,40 @@ function UserJoin() {
             GACHAGACHA!
           </h1>
         </header>
-        <main className={style.main}>
+        <form onSubmit={handleSubmit(onSubmit)} className={style.main}>
           <header className={style.main_header}>
-            <div className={style.main_header_img}>
-              <img className={style.main_img1} src="/images/Camera.svg" alt="profile" />
-              <img className={style.main_img2} src="/images/Camera.svg" alt="Camera" />
-            </div>
+            <label className={style.main_header_label} htmlFor="profile">
+              <img
+                className={style.main_img1}
+                src={user?.profileUrl ? user?.profileUrl : defaultImage}
+                alt="profile"
+              />
+              <img
+                className={style.main_img2}
+                src="/images/Camera.svg"
+                alt="Camera"
+              />
+            </label>
+            <input id="profile" type="file" className={style.main_header_img} {...register("image")}/>
             <button>기본 이미지로 설정</button>
           </header>
           <section className={style.main_main}>
             <div className={style.main_wrapper}>
               <div className={style.main_main_title}>닉네임</div>
               <div className={style.main_main_contents}>
-                <input type="text" placeholder="닉네임"/>
+                <input type="text" value={user?.nickname} placeholder={user?.nickname ? user?.nickname : "닉네임"} {...register("nickname")}/>
                 <button>Enter</button>
               </div>
-              <p className={style.main_main_error}>이미 사용중인 닉네임입니다.</p>
+              <p className={style.main_main_error}>
+                {/* 이미 사용중인 닉네임입니다. */}
+                {errors.nickname?.message}
+              </p>
             </div>
           </section>
-        </main>
+        </form>
       </section>
     </div>
   )
 }
 
-export default UserJoin
+export default UserJoin;
