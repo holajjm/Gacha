aedcee0731a3fca22b0893a5ef597f3619885d1e
holajm <holajjm@naver.com>
2025-01-13 17:51:52 +0900

    Feat: 카카오, 깃허브 소셜 로그인 로직 구현

--- src/pages/User/UserLogin.tsx
+++ src/pages/User/UserLogin.tsx
@@ -1,28 +1,27 @@
 import React from "react"
+// import { useNavigate } from "react-router-dom"
+// import { useStore } from "@store/store";
 import style from "@styles/User/UserLogin.module.css"
-import { useNavigate } from "react-router-dom"
 
 function UserLogin() {
-  const navigate = useNavigate();
-  const fakeLogin = () => {
-    sessionStorage.setItem("fake", "fakeID")
-    alert("임시 로그인 완료")
-    navigate("/minihome")
-  }
-  const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
-  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+  // const {count, increment, decrement} = useStore((state) => state);
+  // const navigate = useNavigate();
+  // const fakeLogin = () => {
+  //   sessionStorage.setItem("fake", "fakeID")
+  //   alert("임시 로그인 완료")
+  //   navigate("/minihome")
+  // }
+  const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI
+  const kakaoURL = `http://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
 
   const kakaoLogin = async () => {
     window.location.href = kakaoURL;
   }
 
+  const githubURL = "https://github.com/login/oauth/authorize?client_id=Iv23lica8LFNbzOrB595";
   const githubLogin = async () => {
-    const response = await fetch(
-      "https://github.com/login/oauth/authorize?client_id=Iv23lica8LFNbzOrB595",
-    )
-    const data = await response.json()
-    console.log(data)
+    window.location.href = githubURL;
   }
   return (
     <div className={style.container}>
@@ -46,7 +45,10 @@ function UserLogin() {
               />
               <p>깃허브로 시작하기</p>
             </button>
-            <button onClick={fakeLogin}>임시 로그인</button>
+            {/* <button onClick={fakeLogin}>임시 로그인</button>
+            <p>{count}</p>
+            <button onClick={() => increment()}>plus</button>
+            <button onClick={() => decrement()}>minus</button> */}
           </div>
         </main>
       </section>
