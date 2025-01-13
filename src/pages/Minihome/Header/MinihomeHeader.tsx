9d3db8d8c67b260aa9b69ed0945fa096c81aa09f
holajm <holajjm@naver.com>
2025-01-13 17:50:25 +0900

    Fix: 미니홈 유저 정보에 따른 데이터 렌더링 영역 수정

--- src/pages/Minihome/Header/MinihomeHeader.tsx
+++ src/pages/Minihome/Header/MinihomeHeader.tsx
@@ -2,8 +2,20 @@ import React, { useState } from "react"
 import style from "@styles/Minihome/Header/MinihomeHeader.module.css"
 import { useNavigate } from "react-router-dom"
 // import MinihomeFollowing from "./MinihomeFollowing";
+interface MiniHomeMainData {
+  followersCnt: number
+  followingCnt: number
+  isOwner: boolean
+  layout: null
+  nickname: string
+  profileImageUrl: string
+  score: number
+  totalVisitorCnt: number
+}
 
-function MinihomeHeader() {
+function MinihomeHeader({minihomeData}:{minihomeData:MiniHomeMainData}) {
+  console.log(minihomeData);
+
   const navigate = useNavigate();
   const [followingClick, setFollowingClick] = useState<boolean>(false);
   const handleFollowingClick = (e:React.MouseEvent<HTMLElement>) => {
@@ -16,21 +28,23 @@ function MinihomeHeader() {
     <header className={style.header}>
       {/* {followingClick ? <MinihomeFollowing /> : null} */}
       <div className={style.header_profile}>
-        <img src="/images/Sample.svg" alt="profile" />
+        <img src={minihomeData?.profileImageUrl} alt="profile" />
       </div>
       <main className={style.header_user}>
         <div className={style.header_user_info}>
-          <p className={style.header_user_name}>olsohee</p>
+          <p className={style.header_user_name}>{minihomeData?.nickname}</p>
           <div className={style.header_info}>
-            <p>랭킹: 상위 5%</p>
-            <p>팔로워 210</p>
-            <p datatype="Following" onClick={handleFollowingClick}>팔로잉 100</p>
+            <p>스코어 {minihomeData?.score}</p>
+            <p>팔로워 {minihomeData?.followersCnt}</p>
+            <p datatype="Following" onClick={handleFollowingClick}>팔로잉 {minihomeData?.followingCnt}</p>
           </div>
         </div>
-        <div className={style.header_bottom}>
-          <button onClick={() => navigate(`/minihome/itembook`)} className={style.header_button}>아이템 북 관리</button>
-          <button onClick={() => navigate(`/minihome/adorn`)} className={style.header_button}>미니홈 꾸미기</button>
-        </div>
+        {minihomeData && minihomeData.isOwner ? (
+          <div className={style.header_bottom}>
+            <button onClick={() => navigate(`/minihome/itembook`)} className={style.header_button}>아이템 북 관리</button>
+            <button onClick={() => navigate(`/minihome/adorn`)} className={style.header_button}>미니홈 꾸미기</button>
+          </div>
+        ) : null}
       </main>
     </header>
   )
