8cd25c4276ff79e058050c432d80adc7d8ad7e62
holajm <holajjm@naver.com>
2025-01-13 17:48:15 +0900

    Fix: 유저 정보에 따른 상단바 메뉴 레이아웃 설정
    유저 로그인 유무에 따른 상단바 메뉴 설정 및 로그 아웃 시 토큰 삭제 로직 구현

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,14 +1,20 @@
 import React from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { useUserStore } from "@store/store";
 import style from "@styles/Layouts/Header.module.css"
 
 function Header() {
-  const navigate = useNavigate()
-
+  const {user} = useUserStore((state) => state);
+  const navigate = useNavigate();
   const handleLogout = () => {
-    sessionStorage.removeItem("fake")
-    alert("로그아웃 완료")
-    navigate("/main")
+    if(confirm("로그아웃 하시겠습니까?")){
+      sessionStorage.removeItem("user");
+      localStorage.removeItem("AccessToken");
+      localStorage.removeItem("RefreshToken");
+      alert("로그아웃 완료");
+      navigate("/main");
+      window.location.reload();
+    }
   }
   return (
     <div className={style.container}>
@@ -16,7 +22,7 @@ function Header() {
         <a className={style.title} href="/main">
           <img src="/images/MainLogo.svg" alt="MainLogo" />
         </a>
-        {sessionStorage.getItem("fake") ? (
+        {user && user?.isNewUser ? (
           <div className={style.link_wrapper}>
             <Link to={"/minihome"} className={style.link}>
               MINIHOME
