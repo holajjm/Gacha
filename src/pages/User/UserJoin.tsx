0a11c50d45db6a4f7b64533496fb92808fb58824
holajm <holajjm@naver.com>
2025-01-14 16:24:31 +0900

    Test: https 프로토콜 확인을 위한 vercel 배포 테스트

--- src/pages/User/UserJoin.tsx
+++ src/pages/User/UserJoin.tsx
@@ -1,51 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "@styles/User/UserJoin.module.css";
 import { useForm } from "react-hook-form";
 import { useUserStore } from "@store/store";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface Data {
   image: string;
   nickname: string;
 }
+interface QueryParams {
+  [key: string]: string;
+}
 function UserJoin() {
-  const {user,setUser} = useUserStore((state) => state);
+  const [loginParams, setLoginParams] = useState<QueryParams>({
+    socialType: "",
+    loginId: "",
+  });
+  const location = useLocation();
+  const getUrlParams = () => {
+    const trim = location.search.replace(/^\?/, "");
+    const obj: QueryParams = {};
+    trim.replace(/([^&=]+)=([^&]*)/g, (match, key, value) => {
+      obj[key] = decodeURIComponent(value);
+      return match;
+    });
+    setLoginParams(obj);
+  };
+  useEffect(() => {
+    getUrlParams();
+  }, []);
+  console.log(loginParams);
+
+  const { user, setUser } = useUserStore((state) => state);
   const navigate = useNavigate();
   console.log(user);
-  
+
   const defaultImage = "/images/Camera.svg";
 
-  const {register, handleSubmit, formState: {errors}} = useForm<Data>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Data>();
 
   const onSubmit = async () => {
-    const response = await fetch("http://61.79.183.245:80/join",{
+    const response = await fetch("https://61.79.183.245:80/join", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        "nickname": user.nickname,
-        "loginType": user.loginType,
-        "loginId": user.loginId,
-        "profileUrl": user.profileUrl
+        nickname: user.nickname,
+        loginType: user.loginType,
+        loginId: user.loginId,
+        profileUrl: user.profileUrl,
       }),
     });
     const data = await response.json();
     console.log(data);
 
-    if(data){
+    if (data) {
       setUser({
         ...user,
         accessToken: data.accessToken,
         refreshToken: data.refreshToken,
-      })
+      });
       localStorage.setItem("AccessToken", JSON.stringify(data.accessToken));
       localStorage.setItem("RefreshToken", JSON.stringify(data.refreshToken));
       alert("회원가입 완료");
       navigate("/main");
     }
-  }
-  
+  };
+
   return (
     <div className={style.container}>
       <section className={style.wrapper}>
@@ -70,14 +96,24 @@ function UserJoin() {
                 alt="Camera"
               />
             </label>
-            <input id="profile" type="file" className={style.main_header_img} {...register("image")}/>
+            <input
+              id="profile"
+              type="file"
+              className={style.main_header_img}
+              {...register("image")}
+            />
             <button>기본 이미지로 설정</button>
           </header>
           <section className={style.main_main}>
             <div className={style.main_wrapper}>
               <div className={style.main_main_title}>닉네임</div>
               <div className={style.main_main_contents}>
-                <input type="text" value={user?.nickname} placeholder={user?.nickname ? user?.nickname : "닉네임"} {...register("nickname")}/>
+                <input
+                  type="text"
+                  value={user?.nickname}
+                  placeholder={user?.nickname ? user?.nickname : "닉네임"}
+                  {...register("nickname")}
+                />
                 <button>Enter</button>
               </div>
               <p className={style.main_main_error}>
@@ -89,7 +125,7 @@ function UserJoin() {
         </form>
       </section>
     </div>
-  )
+  );
 }
 
 export default UserJoin;
--- src/pages/User/UserLogin.tsx
+++ src/pages/User/UserLogin.tsx
@@ -1,7 +1,7 @@
-import React from "react"
+import React from "react";
 // import { useNavigate } from "react-router-dom"
 // import { useStore } from "@store/store";
-import style from "@styles/User/UserLogin.module.css"
+import style from "@styles/User/UserLogin.module.css";
 
 function UserLogin() {
   // const {count, increment, decrement} = useStore((state) => state);
@@ -11,18 +11,36 @@ function UserLogin() {
   //   alert("임시 로그인 완료")
   //   navigate("/minihome")
   // }
-  const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID
-  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI
-  const kakaoURL = `http://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`
+  const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
+  const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   const kakaoLogin = async () => {
     window.location.href = kakaoURL;
-  }
+  };
 
-  const githubURL = "https://github.com/login/oauth/authorize?client_id=Iv23lica8LFNbzOrB595";
+  const githubURL =
+    "https://github.com/login/oauth/authorize?client_id=Iv23lica8LFNbzOrB595";
   const githubLogin = async () => {
     window.location.href = githubURL;
-  }
+  };
+  const handleTest = async () => {
+    const response = await fetch("https://61.79.183.245:80/test", {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": `http://localhost:5173`,
+        "Access-Control-Allow-Credentials": "true",
+      },
+    });
+    const data = await response.json();
+    const cookie = response.headers.get("Set-Cookie");
+    console.log(cookie);
+    console.log(data);
+    
+  };
+
   return (
     <div className={style.container}>
       <section className={style.wrapper}>
@@ -45,6 +63,7 @@ function UserLogin() {
               />
               <p>깃허브로 시작하기</p>
             </button>
+            <button onClick={handleTest}>test</button>
             {/* <button onClick={fakeLogin}>임시 로그인</button>
             <p>{count}</p>
             <button onClick={() => increment()}>plus</button>
@@ -53,7 +72,7 @@ function UserLogin() {
         </main>
       </section>
     </div>
-  )
+  );
 }
 
-export default UserLogin
+export default UserLogin;
