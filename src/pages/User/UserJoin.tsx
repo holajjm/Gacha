29ab22a515c02fb432295ea43a3219c52a8ecbcb
holajm <holajjm@naver.com>
2025-01-15 15:47:04 +0900

    Fix: 회원가입 로직 대폭 수정
    소셜 로그인을 위한 API 요청 body 수정, 전역 상태 관리 로직 수정

--- src/pages/User/UserJoin.tsx
+++ src/pages/User/UserJoin.tsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from "react";
-import style from "@styles/User/UserJoin.module.css";
 import { useForm } from "react-hook-form";
+import { useLocation } from "react-router-dom";
 import { useUserStore } from "@store/store";
-import { useLocation, useNavigate } from "react-router-dom";
+import style from "@styles/User/UserJoin.module.css";
 
 interface Data {
-  image: string;
   nickname: string;
+  profileUrl: FileList;
 }
 interface QueryParams {
   [key: string]: string;
 }
 function UserJoin() {
+  const defaultImage = "/images/Camera.svg";
+  const { setUser } = useUserStore((state) => state);
+
+  const location = useLocation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Data>();
+
   const [loginParams, setLoginParams] = useState<QueryParams>({
     socialType: "",
     loginId: "",
   });
-  const location = useLocation();
+  console.log(loginParams);
+
   const getUrlParams = () => {
     const trim = location.search.replace(/^\?/, "");
     const obj: QueryParams = {};
@@ -26,52 +37,61 @@ function UserJoin() {
     });
     setLoginParams(obj);
   };
+
   useEffect(() => {
     getUrlParams();
   }, []);
-  console.log(loginParams);
 
-  const { user, setUser } = useUserStore((state) => state);
-  const navigate = useNavigate();
-  console.log(user);
-
-  const defaultImage = "/images/Camera.svg";
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<Data>();
-
-  const onSubmit = async () => {
-    const response = await fetch("https://61.79.183.245:80/join", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        nickname: user.nickname,
-        loginType: user.loginType,
-        loginId: user.loginId,
-        profileUrl: user.profileUrl,
-      }),
-    });
-    const data = await response.json();
-    console.log(data);
-
-    if (data) {
-      setUser({
-        ...user,
-        accessToken: data.accessToken,
-        refreshToken: data.refreshToken,
+  const onSubmit = async (formData: Data) => {
+    console.log(formData);
+    
+    try {
+      const response = await fetch("https://61.79.183.245:80/join", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          socialType: loginParams.socialType,
+          loginId: loginParams.loginId,
+          nickname: formData.nickname,
+          profileUrl: formData.profileUrl.length !== 0 ? formData.profileUrl[0].name : "Camera.svg",
+        }),
       });
-      localStorage.setItem("AccessToken", JSON.stringify(data.accessToken));
-      localStorage.setItem("RefreshToken", JSON.stringify(data.refreshToken));
-      alert("회원가입 완료");
-      navigate("/main");
+      console.log(response);
+      
+      if (response) {
+        setUser({
+          socialType: loginParams.socialType,
+          loginId: loginParams.loginId,
+          nickname: formData.nickname,
+          profileUrl: formData.profileUrl.length !== 0 ? formData?.profileUrl[0]?.name : "Camera.svg",
+          accessToken: "",
+          refreshToken: ""
+        })
+        alert("회원가입 완료");
+        window.location.href = response.url;
+      }
+    } catch (error) {
+      console.log(error);
+      
     }
+    // if (data) {
+    //   setUser({
+    //     ...user,
+    //     accessToken: data.accessToken,
+    //     refreshToken: data.refreshToken,
+    //   });
+    //   localStorage.setItem("AccessToken", JSON.stringify(data.accessToken));
+    //   localStorage.setItem("RefreshToken", JSON.stringify(data.refreshToken));
+    //   alert("회원가입 완료");
+    //   navigate("/main");
+    // }
   };
-
+  const handleDefaultImage = () => {
+    console.log(defaultImage);
+    
+  }
   return (
     <div className={style.container}>
       <section className={style.wrapper}>
@@ -87,12 +107,12 @@ function UserJoin() {
             <label className={style.main_header_label} htmlFor="profile">
               <img
                 className={style.main_img1}
-                src={user?.profileUrl ? user?.profileUrl : defaultImage}
+                src={defaultImage}
                 alt="profile"
               />
               <img
                 className={style.main_img2}
-                src="/images/Camera.svg"
+                src={defaultImage}
                 alt="Camera"
               />
             </label>
@@ -100,9 +120,12 @@ function UserJoin() {
               id="profile"
               type="file"
               className={style.main_header_img}
-              {...register("image")}
+              {...register("profileUrl",{required: "프로필 이미지를 선택해주세요."})}
             />
-            <button>기본 이미지로 설정</button>
+            <p className={style.main_main_error}>
+                {errors.profileUrl?.message}
+            </p>
+            <button onClick={handleDefaultImage}>기본 이미지로 설정</button>
           </header>
           <section className={style.main_main}>
             <div className={style.main_wrapper}>
@@ -110,14 +133,12 @@ function UserJoin() {
               <div className={style.main_main_contents}>
                 <input
                   type="text"
-                  value={user?.nickname}
-                  placeholder={user?.nickname ? user?.nickname : "닉네임"}
-                  {...register("nickname")}
+                  placeholder={"닉네임을 입력하세요"}
+                  {...register("nickname",{required: "닉네임을 입력하세요"})}
                 />
                 <button>Enter</button>
               </div>
               <p className={style.main_main_error}>
-                {/* 이미 사용중인 닉네임입니다. */}
                 {errors.nickname?.message}
               </p>
             </div>
