d893282aa89840b94cf78a1f1fc86d070947a96c
holajm <holajjm@naver.com>
2025-01-15 15:40:45 +0900

    Refactor: 불필요한 전역 상태 삭제 및 주석 삭제

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,44 +1,22 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
-interface Store {
-  count: number
-  increment: () => void
-  decrement: () => void
-}
-
-export const useStore = create<Store>((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-}))
-
-// --------------------------------------------------------
-interface User {
-  isNewUser: string
-  loginId: string
-  loginType: string
-  nickname: string
-  profileUrl: string
-  accessToken: string
-  refreshToken: string
+export interface User {
+  // isNewUser: string;
+  loginId: string;
+  socialType: string;
+  nickname: string;
+  profileUrl: string;
+  accessToken: string;
+  refreshToken: string;
 }
 interface UserStore {
-  user: User
-  setUser: (user: User) => void
+  user: User;
+  setUser: (user: User) => void;
 }
 export const useUserStore = create<UserStore>((set) => ({
-  // user: {
-  //   isNewUser: "",
-  //   loginId: "",
-  //   loginType: "",
-  //   nickname: "",
-  //   profileUrl: "",
-  //   accessToken: "",
-  //   refreshToken: "",
-  // },
-  user: JSON.parse(sessionStorage.getItem('user') || 'null'),
+  user: JSON.parse(sessionStorage.getItem("user") || "null"),
   setUser: (user: User) => {
     sessionStorage.setItem("user", JSON.stringify(user));
     set({ user });
   },
-}))
+}));
