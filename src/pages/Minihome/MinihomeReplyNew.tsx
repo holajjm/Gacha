2ad5383dca8131e06688f7b17d6f862cba948ea5
holajm <holajjm@naver.com>
2025-01-21 15:06:00 +0900

    Feat: 댓글 등록 로직 및 즉시 호출 로직 생성

--- src/pages/Minihome/MinihomeReplyNew.tsx
+++ src/pages/Minihome/MinihomeReplyNew.tsx
@@ -1,43 +1,125 @@
-import React, { useRef, useState } from "react";
-import style from "@styles/Minihome/MinihomeReplyNew.module.css";
+import React, { useEffect, useState } from "react";
 import MinihomeReplyItem from "./MinihomeReplyItem";
+import { useForm } from "react-hook-form";
+import { useParams } from "react-router-dom";
+import { useUserStore } from "@store/store";
+import style from "@styles/Minihome/MinihomeReplyNew.module.css";
 
+interface ReplySendData {
+  content: string;
+}
+interface ReplyData {
+  content: string;
+  createAt: string;
+  guestbookId: number;
+  isAuthor: boolean;
+  nickname: string;
+}
+// interface GetReplyData {
+//   content: ReplyData[],
+//   empty: boolean,
+//   first: boolean,
+//   last: boolean,
+//   number: number,
+//   numberOfElements: number,
+//   pageable: {
+//     offset: number,
+//     pageNumber: number,
+//     pageSize: number,
+//     paged: boolean,
+//     sort: {
+//       unsorted: boolean,
+//       sorted: boolean,
+//       empty: boolean
+//     }
+//   }
+//   size: number,
+//   sort: {
+//     unsorted: boolean,
+//     sorted: boolean,
+//     empty: boolean
+//   }
+// }
 function MinihomeReplyNew() {
-  const [input, setInput] = useState<string>("");
-  const [item,setItem] = useState<{id:number,content:string,date:string,time:string}[]>([]);
-  const inputRef = useRef<number>(0);
+  const [replys, setReplys] = useState<ReplyData[]>([]);
+  const { nickname } = useParams<{ nickname: string }>();
+  const { user } = useUserStore((state) => state);
+  // console.log("user :",user);
+  // console.log("nickname :",nickname);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ReplySendData>();
 
-  const handleInput = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setInput(e.target.value);
-  }
+  //useForm의 타입과 handleSubmit에 들어가는 form 제출 함수의 매개변수 타입을 일치 시켜주어야 한다.
 
-  const handleClick = () => {
-    if(input.length !== 0){
-      const newValue = {
-        id: inputRef.current++,
-        content:input,
-        date:new Date().toLocaleDateString(),
-        time: new Date().toLocaleTimeString()
-      }
-      setItem((prev) => [...prev,newValue])
-      setInput("")
+  const onSubmit = async (formData:ReplySendData) => {
+    try {
+      await fetch(`https://222.121.46.20:80/guestbooks/${nickname}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.accessToken}`,
+        },
+        body: JSON.stringify({
+          content: formData.content,
+        }),
+      });
+      getReply();
+    } catch (error) {
+      console.error(error)
     }
-    return
-  }
-  
-  const replyList = item.map(e => <MinihomeReplyItem key={e.id} item={e} />);
+  };
+  const getReply = async () => {
+    try {
+      const response = await fetch(
+        `https://222.121.46.20:80/guestbooks/${nickname}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.accessToken}`,
+          },
+        },
+      );
+      const data = await response.json();
+      // console.log(data);
+      setReplys(data?.content);
+    } catch (error) {
+      console.error(error)
+    }
+  };
 
+  // console.log(replys);
+  
+  useEffect(() => {
+    getReply();
+  }, []);
+  
+  const replyList = replys?.map((e) => (
+    <MinihomeReplyItem key={e.guestbookId} replys={e} getReply={getReply}/>
+  ));
+  
   return (
     <div className={style.main_reply}>
-      <header className={style.main_reply_input}>
-        <input type="text" value={input} onChange={handleInput} placeholder="방명록을 남겨보세요" />
-        <button onClick={handleClick}>등록</button>
-      </header>
-      <main className={style.main_replyList}>
-        {replyList}
-      </main>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className={style.main_reply_input}
+      >
+        <input
+          type="text"
+          placeholder="방명록을 남겨보세요"
+          {...register("content", { required: "내용을 입력해주세요" })}
+        />
+        <button>등록</button>
+        <p className={style.main_replyError}>
+          {errors.content?.message}
+        </p>
+      </form>
+      <main className={style.main_replyList}>{replyList}</main>
     </div>
-  )
+  );
 }
 
-export default MinihomeReplyNew
+export default MinihomeReplyNew;
