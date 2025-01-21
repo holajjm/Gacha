bf9b48d067f352494f5ff8f388ed54f5d2ee00f7
holajm <holajjm@naver.com>
2025-01-21 15:00:27 +0900

    Refactor: Prettier 설정에 맞는 코드 정리

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import alias from "@rollup/plugin-alias";
-import * as path from 'path'
+import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-  plugins: [react(),
+  base: "/",
+  plugins: [
+    react(),
     alias({
       entries: [
-        {find: "@", replacement: path.resolve(__dirname,'src')},
-        {find: "@assets", replacement: path.resolve(__dirname,'src/assets')},
-        {find: "@styles", replacement: path.resolve(__dirname,'src/styles')},
-        {find: "@components", replacement: path.resolve(__dirname,'src/components')},
-        {find: "@layouts", replacement: path.resolve(__dirname,'src/components/layouts')},
-        {find: "@pages", replacement: path.resolve(__dirname,'src/pages')},
-        {find: "@Explore", replacement: path.resolve(__dirname,'src/Explore')},
-        {find: "@Gacha", replacement: path.resolve(__dirname,'src/Gacha')},
-        {find: "@Main", replacement: path.resolve(__dirname,'src/Main')},
-        {find: "@User", replacement: path.resolve(__dirname,'src/User')},
-        {find: "@store", replacement: path.resolve(__dirname,'src/store')},
-      ]
-    })
+        { find: "@", replacement: path.resolve(__dirname, "src") },
+        { find: "@assets", replacement: path.resolve(__dirname, "src/assets") },
+        { find: "@styles", replacement: path.resolve(__dirname, "src/styles") },
+        {
+          find: "@components",
+          replacement: path.resolve(__dirname, "src/components"),
+        },
+        {
+          find: "@layouts",
+          replacement: path.resolve(__dirname, "src/components/layouts"),
+        },
+        { find: "@pages", replacement: path.resolve(__dirname, "src/pages") },
+        {
+          find: "@Explore",
+          replacement: path.resolve(__dirname, "src/Explore"),
+        },
+        { find: "@Gacha", replacement: path.resolve(__dirname, "src/Gacha") },
+        { find: "@Main", replacement: path.resolve(__dirname, "src/Main") },
+        { find: "@User", replacement: path.resolve(__dirname, "src/User") },
+        { find: "@store", replacement: path.resolve(__dirname, "src/store") },
+      ],
+    }),
   ],
-})
+});
