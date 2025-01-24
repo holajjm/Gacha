4eb600743e6edfb20034601c384e2d8a10a766b5
holajm <holajjm@naver.com>
2025-01-24 17:42:47 +0900

    Feat: 이미지 호출 로직 구현

--- src/pages/Minihome/Adorn/MinihomeAdornItem.tsx
+++ src/pages/Minihome/Adorn/MinihomeAdornItem.tsx
@@ -1,25 +1,34 @@
-import React from "react";
-import style from "@styles/Minihome/Adorn/MinihomeAdornItem.module.css";
+import React, { useEffect, useState } from 'react'
 
-function MinihomeAdornItem() {
+interface ItemData {
+  imageUrl: string;
+  itemCnt: number;
+  itemGrade: string;
+  itemId: number,
+  itemName: string,
+  userItemIds: null,
+}
+
+function MinihomeAdornItem({data}:{data:ItemData}) {
+  const [imageList, setImageList] = useState<string>();
+      const image = async () => {
+        const response = await fetch(`https://222.121.46.20:80${data?.imageUrl}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "image/png, image/jif"
+          },
+        });
+        const blob = await response.blob();
+        const imageObjUrl = URL.createObjectURL(blob);
+        setImageList(imageObjUrl);
+      };
+      useEffect(() => {
+        image();
+      }, []);
   return (
-    <section className={style.main}>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-    </section>
+    <div>
+      <img src={imageList} alt="" />
+    </div>
   )
 }
 
