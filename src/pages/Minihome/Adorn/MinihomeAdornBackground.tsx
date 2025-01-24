044ce14c468f7dcf1e675c88e29d5f9d7417d541
holajm <holajjm@naver.com>
2025-01-24 17:46:14 +0900

    Feat: 배경 요소 호출 로직 구현

--- src/pages/Minihome/Adorn/MinihomeAdornBackground.tsx
+++ src/pages/Minihome/Adorn/MinihomeAdornBackground.tsx
@@ -1,29 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "@styles/Minihome/Adorn/MinihomeAdornBackground.module.css";
+import { useUserStore } from "@store/store";
+import MinihomeAdornBackgroundItem from "./MinihomeAdornBackgroundItem";
+
+interface BackgroundItemData {
+  backgroundId: number,
+  imageUrl: string
+}
 
 function MinihomeAdornBackground() {
+  const {user} = useUserStore((state) => state);
+  const [backgroundItemList, setBackgroundItemList] = useState<BackgroundItemData[]>([]);
+  const getBackgroundItems = async () => {
+    const response = await fetch(`https://222.121.46.20:80/backgrounds/${user?.nickname}`,{
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user?.accessToken}`
+      }
+    })
+    const data = await response.json();
+    setBackgroundItemList(data);
+  }
+  useEffect(() => {
+    getBackgroundItems()
+  },[])
+  console.log(backgroundItemList);
+  const backgroundItems = backgroundItemList.map(e => <MinihomeAdornBackgroundItem key={e.backgroundId} data={e}/>)
   return (
     <section className={style.main}>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
-      <div>3</div>
+      {backgroundItems}
     </section>
   )
 }
