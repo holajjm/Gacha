81fe337b6b6fe649fb5388f6ffa9c76ca0a6b40a
holajm <holajjm@naver.com>
2025-01-24 17:40:59 +0900

    Feat: 이미지 호출 로직 구현

--- src/pages/Market/MarketSellItem.tsx
+++ src/pages/Market/MarketSellItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 interface Item {
   userItemId: number,
@@ -12,9 +12,24 @@ function MarketSellItem({item,onSelect}:{item:Item,onSelect:(item:Item) => void}
   const handleClick = () => {
     onSelect(item)
   }
+  const [imageList, setImageList] = useState<string>();
+    const image = async () => {
+      const response = await fetch(`https://222.121.46.20:80${item?.imageUrl}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "image/png, image/jif"
+        },
+      });
+      const blob = await response.blob();
+      const imageObjUrl = URL.createObjectURL(blob);
+      setImageList(imageObjUrl);
+    };
+    useEffect(() => {
+      image();
+    }, []);
   return (
     <div onClick={handleClick}>
-      <img src={item?.imageUrl} alt="sample" />
+      <img src={imageList} alt="sample" />
     </div>
   );
 }
