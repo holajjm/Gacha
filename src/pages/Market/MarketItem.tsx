ea5518065e14d70448c221b4cc1952c660381ecb
holajm <holajjm@naver.com>
2025-01-24 13:33:14 +0900

    Feat: 판매중인 아이템 목록 이미지 렌더링 로직 구현

--- src/pages/Market/MarketItem.tsx
+++ src/pages/Market/MarketItem.tsx
@@ -1,12 +1,35 @@
-import React from "react"
-import style from "@styles/Market/MarketItem.module.css"
+import React, { useEffect, useState } from "react";
 
-function MarketItem() {
-  return (
-    <div  className={style.item}>
-      <img src="/images/Sample.svg" alt="cat" />
-    </div>
-  )
+import style from "@styles/Market/MarketItem.module.css";
+
+interface MarketItemData {
+  hasStock: string;
+  imageUrl: string;
+  itemId: number;
 }
 
-export default MarketItem
+function MarketItem({ data }: { data: MarketItemData }) {
+  const [imageList, setImageList] = useState<string>();
+  const image = async () => {
+    const response = await fetch(`https://222.121.46.20:80${data?.imageUrl}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "image/png, image/jif"
+      },
+    });
+    const blob = await response.blob();
+    const imageObjUrl = URL.createObjectURL(blob);
+    setImageList(imageObjUrl);
+  };
+  useEffect(() => {
+    image();
+  }, []);
+  
+  return (
+    <div className={style.item}>
+      <img src={imageList} alt="cat" />
+    </div>
+  );
+}
+
+export default MarketItem;
