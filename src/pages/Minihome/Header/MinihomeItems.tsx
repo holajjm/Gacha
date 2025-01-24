b4b2c65b38c6250f0ada35e0c7161fba037efeab
holajm <holajjm@naver.com>
2025-01-24 17:43:31 +0900

    Feat: 이미지 호출 로직 구현

--- src/pages/Minihome/Header/MinihomeItems.tsx
+++ src/pages/Minihome/Header/MinihomeItems.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "@styles/Minihome/Header/MinihomeItem.module.css";
 
 interface ItemBookData {
@@ -11,9 +11,26 @@ interface ItemBookData {
 }
 
 function MinihomeItems({data}:{data:ItemBookData}) {
+  const [imageList, setImageList] = useState<string>();
+    const image = async () => {
+      const response = await fetch(`https://222.121.46.20:80${data?.imageUrl}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "image/png, image/jif"
+        },
+      });
+      const blob = await response.blob();
+      const imageObjUrl = URL.createObjectURL(blob);
+      setImageList(imageObjUrl);
+    };
+    useEffect(() => {
+      image();
+    }, []);
   return (
-    <div>
-      <img src={data?.imageUrl} alt="sample" />
+    <div className={style.wrapper}>
+      <div className={style.img}>
+        <img src={imageList} alt="sample" />
+      </div>
       <div className={style.item}>
         <p>{data?.itemGrade}</p>
         <p>{data?.itemName} / {data?.itemCnt}개</p>
