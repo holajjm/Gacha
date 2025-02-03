e0f943017fbfb3f79fadf840fa3e2daee4cad785
holajm <holajjm@naver.com>
2025-02-03 17:50:27 +0900

    Feat: 마켓 메인 화면 모달 화면 생성
    하위 컴포넌트로부터 받은 데이터를 모달 컴포넌트로 전달해주는 로직 생성, 클릭 상태와 클릭된 요소의 데이터 전달

--- src/pages/Market/MarketMain.tsx
+++ src/pages/Market/MarketMain.tsx
@@ -1,38 +1,52 @@
-import React, { useEffect, useState } from "react"
-import style from "@styles/Market/MarketMain.module.css"
-import { useNavigate } from "react-router-dom"
-import MarketItem from "./MarketItem"
-import { useUserStore } from "@store/store"
-// import MarketItemModal from "./MarketItemModal";
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUserStore } from "@store/store";
+
+import MarketItem from "./MarketItem";
+import MarketItemModal from "./MarketItemModal";
+import style from "@styles/Market/MarketMain.module.css";
 
 interface MarketItemData {
-  hasStock: string,
-  imageUrl: string,
-  itemId: number
+  hasStock: string;
+  imageUrl: string;
+  itemId: number;
 }
 
 function MarketMain() {
-  const {user} = useUserStore((state) => state);
-  const [itemList, setItemList] = useState<MarketItemData[]>([])
+  const { user } = useUserStore((state) => state);
+  const [itemList, setItemList] = useState<MarketItemData[]>([]);
+  const [clicked, setClicked] = useState<boolean>(false);
+  const [clickItemId, setClickItemId] = useState<number>(0);
+
   const navigate = useNavigate();
   useEffect(() => {
     const getMarketItem = async () => {
-      const response = await fetch("https://222.121.46.20:80/products",{
+      const response = await fetch("https://222.121.46.20:80/products", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${user?.accessToken}`
-        }
-      })
+          Authorization: `Bearer ${user?.accessToken}`,
+        },
+      });
       const data = await response.json();
       setItemList(data);
-    }
+    };
     getMarketItem();
-  },[])
-  // console.log(itemList);
-  
-  const renderItemList = itemList.map(e => <MarketItem key={e.itemId} data={e}/>)
-  
+  }, []);
+
+  const handleClicked = () => {
+    setClicked(true)
+  }
+  const handleModalClicked = () => {
+    setClicked(false)
+  }
+  const handleClickItemId = useCallback((itemId: number) => {
+    setClickItemId(itemId);
+  }, []);
+  const renderItemList = itemList.map((e) => (
+    <MarketItem key={e.itemId} data={e} onSelect={handleClickItemId} onClick={handleClicked}/>
+  ));
+
   return (
     <div className={style.container}>
       <div className={style.coin}>
@@ -43,13 +57,13 @@ function MarketMain() {
         <div className={style.background}></div>
         <aside className={style.aside}>
           <button
-            onClick={() => navigate("/market/myitem")}
+            onClick={() => navigate("/market/mysellingitem")}
             className={style.aside_button}
           >
             내 판매 목록
           </button>
           <button
-            onClick={() => navigate("/market/sell")}
+            onClick={() => navigate("/market/enroll")}
             className={style.aside_button}
           >
             내 상품 등록
@@ -65,15 +79,13 @@ function MarketMain() {
             <button>D등급</button>
           </nav>
           <section className={style.main_items}>
-            <div className={style.main_items_wrapper}>
-              {renderItemList}
-            </div>
+            <div className={style.main_items_wrapper}>{renderItemList}</div>
           </section>
         </main>
       </section>
-      {/* <MarketItemModal /> */}
+      {clicked ? <MarketItemModal clickItemId={clickItemId} onClick={handleModalClicked}/> : null}
     </div>
-  )
+  );
 }
 
-export default MarketMain
+export default MarketMain;
