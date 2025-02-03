162436685df9e10701d77a95f8bd4d3f44c319c3
holajm <holajjm@naver.com>
2025-02-03 17:51:29 +0900

    Feat: 선택된 요소의 아이디 값과 클릭 상태값 상위 컴포넌트로 전달

--- src/pages/Market/MarketItem.tsx
+++ src/pages/Market/MarketItem.tsx
@@ -8,13 +8,21 @@ interface MarketItemData {
   itemId: number;
 }
 
-function MarketItem({ data }: { data: MarketItemData }) {
+function MarketItem({
+  data,
+  onSelect,
+  onClick,
+}: {
+  data: MarketItemData;
+  onSelect: (itemId: number) => void;
+  onClick: () => void;
+}) {
   const [imageList, setImageList] = useState<string>();
   const image = async () => {
     const response = await fetch(`https://222.121.46.20:80${data?.imageUrl}`, {
       method: "GET",
       headers: {
-        "Content-Type": "image/png, image/jif"
+        "Content-Type": "image/png, image/jif",
       },
     });
     const blob = await response.blob();
@@ -24,10 +32,13 @@ function MarketItem({ data }: { data: MarketItemData }) {
   useEffect(() => {
     image();
   }, []);
-  
+  const handleClick = () => {
+    onSelect(data?.itemId);
+    onClick();
+  };
   return (
-    <div className={style.item}>
-      <img src={imageList} alt="cat" />
+    <div onClick={handleClick} className={style.item}>
+      <img src={imageList} alt="item" />
     </div>
   );
 }
