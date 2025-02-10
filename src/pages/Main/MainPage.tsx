f026ad52341ff0f7b55d5aa02c4a0d9b48e9168c
holajm <holajjm@naver.com>
2025-02-10 13:27:10 +0900

    Refactor: 임시 구현한 뽑기 로직 삭제

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
+
 import style from "@styles/Main/MainPage.module.css";
 
 function MainPage() {
   return (
     <div className={style.container}>
-      <section className={style.wrapper}>
-        MainPage
-      </section>
+      <section className={style.wrapper}>MainPage</section>
     </div>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
