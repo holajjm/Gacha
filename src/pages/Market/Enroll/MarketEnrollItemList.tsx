f9dfb73dafe768c52e25f3ee9fb96261117964dc
holajm <holajjm@naver.com>
2025-02-21 15:16:31 +0900

    Feat: 서버 API 환경 변수 생성 및 페이지네이션 구현

--- src/pages/Market/Enroll/MarketEnrollItemList.tsx
+++ src/pages/Market/Enroll/MarketEnrollItemList.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback, useEffect, useState } from "react";
-import MarketEnrollPreview from "./MarketEnrollPreview";
 import { useUserStore } from "@store/store";
 
-import style from "@styles/Market/Enroll/MarketEnrollItemList.module.css";
+import MarketEnrollPreview from "./MarketEnrollPreview";
 import MarketEnrollItem from "./MarketEnrollItem";
+import style from "@styles/Market/Enroll/MarketEnrollItemList.module.css";
 
 interface Item {
   userItemId: number;
@@ -14,6 +14,7 @@ interface Item {
 }
 
 function MarketSellItemList() {
+  const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
   const [itemList, setItemList] = useState<Item[]>([]);
   const [selectedItem, setSelectedItem] = useState<Item>({
@@ -23,20 +24,42 @@ function MarketSellItemList() {
     price: 0,
     imageUrl: "",
   });
+  const [pageNum, setPageNum] = useState<number>(0);
+  const [currentPage, setCurrentPage] = useState<number>(0);
   const getMyItemList = async () => {
-    const response = await fetch("https://222.121.46.20:80/items/me/forSale", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user?.accessToken}`,
+    const response = await fetch(
+      `${SERVER_API}/items/me/forSale?sort=createdAt,desc&page=${currentPage}&size=10`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.accessToken}`,
+        },
       },
-    });
+    );
     const data = await response.json();
     setItemList(data?.content);
+    setPageNum(Math.floor(data?.totalElements / 10) + 1);
   };
   useEffect(() => {
     getMyItemList();
-  }, []);
+  }, [currentPage]);
+
+  const pageNumList = () => {
+    const numList = [];
+    for (let i = 0; i < pageNum; i++) {
+      numList.push(
+        <li
+          className={currentPage == i ? style.active_pageNum : style.pageNum}
+          key={i}
+          onClick={() => setCurrentPage(i)}
+        >
+          {i + 1}
+        </li>,
+      );
+    }
+    return numList;
+  };
 
   const handleItemClick = useCallback((item: Item) => {
     setSelectedItem(item);
@@ -60,6 +83,7 @@ function MarketSellItemList() {
         <header className={style.main_header}>내 아이템</header>
         <section className={style.main_body}>{myItemList}</section>
       </main>
+      <ul className={style.pageList}>{pageNumList()}</ul>
     </section>
   );
 }
