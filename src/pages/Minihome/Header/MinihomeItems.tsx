8219bb9e2e0c215633b5aa88e6da812273e31581
holajm <holajjm@naver.com>
2025-02-21 15:21:06 +0900

    Feat: 이미지 호출 커스텀 훅 사용

--- src/pages/Minihome/Header/MinihomeItems.tsx
+++ src/pages/Minihome/Header/MinihomeItems.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import style from "@styles/Minihome/Header/MinihomeItem.module.css";
+
 import useImage from "@hooks/useImage";
+import style from "@styles/Minihome/Header/MinihomeItem.module.css";
 
 interface ItemBookData {
   imageUrl: string;
@@ -12,22 +13,6 @@ interface ItemBookData {
 }
 
 function MinihomeItems({data}:{data:ItemBookData}) {
-  // const SERVER_API = import.meta.env.VITE_SERVER_API;
-  // const [imageList, setImageList] = useState<string>();
-  //   const image = async () => {
-  //     const response = await fetch(`${SERVER_API}${data?.imageUrl}`, {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "image/png, image/jif"
-  //       },
-  //     });
-  //     const blob = await response.blob();
-  //     const imageObjUrl = URL.createObjectURL(blob);
-  //     setImageList(imageObjUrl);
-  //   };
-  //   useEffect(() => {
-  //     image();
-  //   }, []);
   return (
     <div className={style.wrapper}>
       <div className={style.img}>
