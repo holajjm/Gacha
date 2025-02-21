85b04218c927207d9c7224d9bea382db76c87d1f
holajm <holajjm@naver.com>
2025-02-21 15:17:04 +0900

    Feat: 이미지 호출 커스텀 훅 사용 및 코드 정리

--- src/pages/Market/Enroll/MarketEnrollPreview.tsx
+++ src/pages/Market/Enroll/MarketEnrollPreview.tsx
@@ -1,63 +1,76 @@
-import React, { useEffect, useState } from "react";
-import style from "@styles/Market/Enroll/MarketEnrollPreview.module.css"
+import React from "react";
+import style from "@styles/Market/Enroll/MarketEnrollPreview.module.css";
 import { useUserStore } from "@store/store";
+import useImage from "@hooks/useImage";
 
 interface Item {
-  userItemId: number,
-  name: string,
-  grade: string,
-  price: number,
-  imageUrl: string
+  userItemId: number;
+  name: string;
+  grade: string;
+  price: number;
+  imageUrl: string;
 }
 
-function MarketSellPreview({item}:{item:Item}) {
-  const {user} = useUserStore((state => state))  ;
+function MarketSellPreview({ item }: { item: Item }) {
+  const SERVER_API = import.meta.env.VITE_SERVER_API;
+  const { user } = useUserStore((state) => state);
   const enrollItem = async () => {
     try {
-      await fetch("https://222.121.46.20:80/products",{
+      await fetch(`${SERVER_API}/products`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${user?.accessToken}`
+          Authorization: `Bearer ${user?.accessToken}`,
         },
         body: JSON.stringify({
-          userItemId: item?.userItemId
-        })
+          userItemId: item?.userItemId,
+        }),
       });
-      alert("상품이 등록되었습니다.")
+      alert("상품이 등록되었습니다.");
     } catch (error) {
       console.error(error);
     }
-  }
-  const [imageList, setImageList] = useState<string>();
-      const image = async () => {
-        const response = await fetch(`https://222.121.46.20:80${item?.imageUrl}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "image/png, image/jif"
-          },
-        });
-        const blob = await response.blob();
-        const imageObjUrl = URL.createObjectURL(blob);
-        setImageList(imageObjUrl);
-      };
-      useEffect(() => {
-        image();
-      }, [item]);
+  };
+  // const [imageList, setImageList] = useState<string>();
+  // const image = async () => {
+  //   const response = await fetch(`${SERVER_API}${item?.imageUrl}`, {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "image/png, image/jif",
+  //     },
+  //   });
+  //   const blob = await response.blob();
+  //   const imageObjUrl = URL.createObjectURL(blob);
+  //   setImageList(imageObjUrl);
+  // };
+  // useEffect(() => {
+  //   image();
+  // }, [item]);
   return (
     <div className={style.header}>
       <div className={style.header_background}></div>
       <div className={style.header_image}>
-        <img src={imageList} alt="image" />
+        <img src={useImage(item?.imageUrl)} alt="image" />
       </div>
       <div className={style.header_item}>
-        <p>Name: {item?.name}</p>
-        <p>Grade: {item?.grade}</p>
-        <p>Price: {item?.price}</p>
-        <button onClick={enrollItem} className={style.header_button}>판매 등록</button>
+        <div className={style.header_wrapper}>
+          <p>
+            <span>Name:</span> <strong>{item?.name ? item?.name : "-"}</strong>
+          </p>
+          <p>
+            <span>Grade:</span>{" "}
+            <strong>{item?.grade ? item?.grade : "-"}</strong>
+          </p>
+          <p>
+            <span>Price:</span> <strong>{item?.price ? item?.price : 0}</strong>
+          </p>
+          <button onClick={enrollItem} className={style.header_button}>
+            판매 등록
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MarketSellPreview
+export default MarketSellPreview;
