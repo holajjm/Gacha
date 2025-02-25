a989073d22c41fba7690139e9378902175986c52
holajm <holajjm@naver.com>
2025-02-25 14:40:58 +0900

    Feat: 버튼 공용 컴포넌트 사용 & Fix: 서버 데이터 로직 변경에 따른 로직 수정

--- src/pages/Market/Enroll/MarketEnrollPreview.tsx
+++ src/pages/Market/Enroll/MarketEnrollPreview.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import style from "@styles/Market/Enroll/MarketEnrollPreview.module.css";
 import { useUserStore } from "@store/store";
 import useImage from "@hooks/useImage";
+import Button from "@components/Button";
 
 interface Item {
-  userItemId: number;
-  name: string;
-  grade: string;
-  price: number;
   imageUrl: string;
+  itemCnt: number
+  itemGrade: string;
+  itemId: number;
+  itemName: string;
+  price: number;
+  stock: number
 }
 
 function MarketSellPreview({ item }: { item: Item }) {
@@ -23,7 +26,7 @@ function MarketSellPreview({ item }: { item: Item }) {
           Authorization: `Bearer ${user?.accessToken}`,
         },
         body: JSON.stringify({
-          userItemId: item?.userItemId,
+          userItemId: item?.itemId,
         }),
       });
       alert("상품이 등록되었습니다.");
@@ -31,21 +34,6 @@ function MarketSellPreview({ item }: { item: Item }) {
       console.error(error);
     }
   };
-  // const [imageList, setImageList] = useState<string>();
-  // const image = async () => {
-  //   const response = await fetch(`${SERVER_API}${item?.imageUrl}`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "image/png, image/jif",
-  //     },
-  //   });
-  //   const blob = await response.blob();
-  //   const imageObjUrl = URL.createObjectURL(blob);
-  //   setImageList(imageObjUrl);
-  // };
-  // useEffect(() => {
-  //   image();
-  // }, [item]);
   return (
     <div className={style.header}>
       <div className={style.header_background}></div>
@@ -55,18 +43,21 @@ function MarketSellPreview({ item }: { item: Item }) {
       <div className={style.header_item}>
         <div className={style.header_wrapper}>
           <p>
-            <span>Name:</span> <strong>{item?.name ? item?.name : "-"}</strong>
+            <span>Name:</span> <strong>{item?.itemName ? item?.itemName : "-"}</strong>
           </p>
           <p>
             <span>Grade:</span>{" "}
-            <strong>{item?.grade ? item?.grade : "-"}</strong>
+            <strong>{item?.itemGrade ? item?.itemGrade : "-"}</strong>
           </p>
           <p>
             <span>Price:</span> <strong>{item?.price ? item?.price : 0}</strong>
           </p>
-          <button onClick={enrollItem} className={style.header_button}>
-            판매 등록
-          </button>
+          <Button
+            text={"판매 등록"}
+            width={"6rem"}
+            onClick={enrollItem}
+            // className={style.header_button}
+          ></Button>
         </div>
       </div>
     </div>
