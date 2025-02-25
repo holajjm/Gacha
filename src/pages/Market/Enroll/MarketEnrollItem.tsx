758da01056eba34b91497450dde081ce9b29c53b
holajm <holajjm@naver.com>
2025-02-25 14:39:39 +0900

    Fix: 서버 데이터 로직 변경에 따른 로직 수정

--- src/pages/Market/Enroll/MarketEnrollItem.tsx
+++ src/pages/Market/Enroll/MarketEnrollItem.tsx
@@ -2,11 +2,13 @@ import useImage from "@hooks/useImage";
 import React from "react";
 
 interface Item {
-  userItemId: number;
-  name: string;
-  grade: string;
-  price: number;
   imageUrl: string;
+  itemCnt: number;
+  itemGrade: string;
+  itemId: number;
+  itemName: string;
+  price: number;
+  stock: number;
 }
 
 function MarketEnrollItem({
@@ -19,22 +21,6 @@ function MarketEnrollItem({
   const handleClick = () => {
     onSelect(item);
   };
-  // const SERVER_API = import.meta.env.VITE_SERVER_API;
-  // const [imageList, setImageList] = useState<string>();
-  // const image = async () => {
-  //   const response = await fetch(`${SERVER_API}${item?.imageUrl}`, {
-  //     method: "GET",
-  //     headers: {
-  //       "Content-Type": "image/png, image/jif",
-  //     },
-  //   });
-  //   const blob = await response.blob();
-  //   const imageObjUrl = URL.createObjectURL(blob);
-  //   setImageList(imageObjUrl);
-  // };
-  // useEffect(() => {
-  //   image();
-  // }, []);
   return (
     <div onClick={handleClick}>
       <img src={useImage(item?.imageUrl)} alt="sample" />
