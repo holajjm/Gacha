a13a1af0a1258ffeb797dc220e64c15485df69e1
holajm <holajjm@naver.com>
2025-02-26 17:43:23 +0900

    Feat: 페이지 타이틀 커스텀 훅 사용

--- src/pages/Market/Enroll/MarketEnroll.tsx
+++ src/pages/Market/Enroll/MarketEnroll.tsx
@@ -2,8 +2,10 @@ import React from "react"
 import style from "@styles/Market/Enroll/MarketEnroll.module.css"
 import MarketEnrollItemList from "./MarketEnrollItemList"
 import Coin from "@components/Coin"
+import usePageTitle from "@hooks/usePageTitle"
 
 function MarketEnroll() {
+  usePageTitle("마켓 - 내 상품 등록");
   return (
     <div className={style.container}>
       <Coin />
