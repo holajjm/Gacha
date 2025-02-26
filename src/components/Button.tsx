b5e6ef0fc019c8fc31761346e0b467dfc4385e98
holajm <holajjm@naver.com>
2025-02-26 13:41:13 +0900

    Refactor: props 텍스트 타입 변경

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { ReactNode } from "react";
 
 import style from "@styles/Layouts/Button.module.css";
 
-function Button({text,width,onClick}:{text:string,width:string,onClick:() => void}) {
+function Button({text,width,onClick}:{text:ReactNode,width:string,onClick:() => void}) {
   return <button style={{width: `${width}`}} className={style.backBtn} onClick={onClick}>{text}</button>;
 }
 
