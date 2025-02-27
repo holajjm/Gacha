e91c448ba2a53259871280c1fbc4289f98fc73b0
holajm <holajjm@naver.com>
2025-02-27 13:45:55 +0900

    Style: 코드 정리

--- src/pages/Minihome/Adorn/MinihomeAdornItem.tsx
+++ src/pages/Minihome/Adorn/MinihomeAdornItem.tsx
@@ -11,22 +11,6 @@ interface ItemData {
 }
 
 function MinihomeAdornItem({data}:{data:ItemData}) {
-  // const SERVER_API = import.meta.env.VITE_SERVER_API;
-  // const [imageList, setImageList] = useState<string>();
-  //     const image = async () => {
-  //       const response = await fetch(`${SERVER_API}${data?.imageUrl}`, {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "image/png, image/jif"
-  //         },
-  //       });
-  //       const blob = await response.blob();
-  //       const imageObjUrl = URL.createObjectURL(blob);
-  //       setImageList(imageObjUrl);
-  //     };
-  //     useEffect(() => {
-  //       image();
-  //     }, []);
   return (
     <div>
       <img src={useImage(data?.imageUrl)} alt="" />
