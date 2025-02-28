cebdcb0470a02dd23e07e86dd6c1c7a430a58df3
holajm <holajjm@naver.com>
2025-02-28 17:11:49 +0900

    Feat: 회원 가입 로직 수정
    1. 백엔드 서버 구조 변경에 따른 회원 가입 시 전달하는 formdata 형태 수정
    2. 프로필 이미지 등록 시 미리보기 사진 구현 및 기본 이미지 설정 시 기본 이미지 미리보기 구현
    
    -> 프로필 이미지 서버 측으로 정상 전달 오류 발생. 개선 요망

--- src/pages/User/UserJoin.tsx
+++ src/pages/User/UserJoin.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
 import { useLocation } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
 import { useUserStore } from "@store/store";
 import style from "@styles/User/UserJoin.module.css";
 
@@ -11,23 +12,24 @@ interface Data {
 interface QueryParams {
   [key: string]: string;
 }
-function UserJoin() {
-  const defaultImage = "/images/Camera.svg";
-  const { setUser } = useUserStore((state) => state);
 
+function UserJoin() {
+  const SERVER_API = import.meta.env.VITE_SERVER_API;
+  const { setUser } = useUserStore((state) => state);
   const location = useLocation();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    clearErrors,
+    setValue,
   } = useForm<Data>();
 
+  // 파라미터로 전달받은 socialId,loginId 값 디코딩하여 객체로 사용
   const [loginParams, setLoginParams] = useState<QueryParams>({
     socialType: "",
     loginId: "",
   });
-  console.log(loginParams);
-
   const getUrlParams = () => {
     const trim = location.search.replace(/^\?/, "");
     const obj: QueryParams = {};
@@ -37,61 +39,85 @@ function UserJoin() {
     });
     setLoginParams(obj);
   };
-
   useEffect(() => {
     getUrlParams();
   }, []);
 
+  // 회원가입 폼 양식 전달 로직
   const onSubmit = async (formData: Data) => {
     console.log(formData);
-    
+    const form = new FormData();
+    form.append(
+      "data",
+      new Blob(
+        [
+          JSON.stringify({
+            socialType: loginParams.socialType,
+            loginId: loginParams.loginId,
+            nickname: formData.nickname,
+          }),
+        ],
+        { type: "application/json" },
+      ),
+    );
+    if (!defaultImage && formData?.profileUrl.length > 0) {
+      form.append("profileImageFile", imagePreview);
+    } else {
+      form.append("profileImageFile", "");
+    }
     try {
-      const response = await fetch("https://222.121.46.20:80/join", {
+      const response = await fetch(`${SERVER_API}/join`, {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          socialType: loginParams.socialType,
-          loginId: loginParams.loginId,
-          nickname: formData.nickname,
-          profileUrl: formData.profileUrl.length !== 0 ? formData.profileUrl[0].name : "Camera.svg",
-        }),
+        body: form,
       });
-      console.log(response);
-      
-      if (response) {
+      if (response?.status === 200) {
         setUser({
           socialType: loginParams.socialType,
           loginId: loginParams.loginId,
           nickname: formData.nickname,
-          profileUrl: formData.profileUrl.length !== 0 ? formData?.profileUrl[0]?.name : "Camera.svg",
+          profileUrl:
+            formData.profileUrl.length > 0 ? formData?.profileUrl[0]?.name : "",
           accessToken: "",
-          refreshToken: ""
-        })
+          refreshToken: "",
+        });
         alert("회원가입 완료");
         window.location.href = response.url;
       }
     } catch (error) {
       console.log(error);
-      
     }
-    // if (data) {
-    //   setUser({
-    //     ...user,
-    //     accessToken: data.accessToken,
-    //     refreshToken: data.refreshToken,
-    //   });
-    //   localStorage.setItem("AccessToken", JSON.stringify(data.accessToken));
-    //   localStorage.setItem("RefreshToken", JSON.stringify(data.refreshToken));
-    //   alert("회원가입 완료");
-    //   navigate("/main");
-    // }
   };
+
+  // // 기본 이미지 설정 및 이미지 미리보기 구현
+  const [defaultImage, setDefaultImage] = useState<boolean>(false); // 기본 이미지 여부
+  const [imagePreview, setImagePreview] = useState<string>(
+    "/images/LightDefaultImage.png",
+  );
+  useEffect(() => {
+    if (defaultImage) {
+      setImagePreview("/images/LightDefaultImage.png");
+    }
+  }, [defaultImage]);
+
   const handleDefaultImage = () => {
-    console.log(defaultImage);
-    
-  }
+    clearErrors("profileUrl"); //useForm의 clearErrors 메서드를 통해 특정 input 값의 에러만 삭제
+    setDefaultImage(true);
+    setValue("profileUrl", new DataTransfer().files);
+  };
+  const handlePreview = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.files?.[0]);
+    const selectedFile = e.target.files ? e.target.files[0] : null;
+    if (selectedFile) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+      reader.readAsDataURL(selectedFile);
+      setDefaultImage(false);
+    }
+  };
+  // console.log(imagePreview);
+  
   return (
     <div className={style.container}>
       <section className={style.wrapper}>
@@ -107,12 +133,12 @@ function UserJoin() {
             <label className={style.main_header_label} htmlFor="profile">
               <img
                 className={style.main_img1}
-                src={defaultImage}
+                src={imagePreview}
                 alt="profile"
               />
               <img
                 className={style.main_img2}
-                src={defaultImage}
+                src="/images/Camera.svg"
                 alt="Camera"
               />
             </label>
@@ -120,12 +146,15 @@ function UserJoin() {
               id="profile"
               type="file"
               className={style.main_header_img}
-              {...register("profileUrl",{required: "프로필 이미지를 선택해주세요."})}
+              {...register("profileUrl", {
+                required: "프로필 이미지를 선택해주세요.",
+              })}
+              onChange={handlePreview}
             />
             <p className={style.main_main_error}>
-                {errors.profileUrl?.message}
+              {errors ? errors.profileUrl?.message : ""}
             </p>
-            <button onClick={handleDefaultImage}>기본 이미지로 설정</button>
+            <div onClick={handleDefaultImage}>기본 이미지로 하기</div>
           </header>
           <section className={style.main_main}>
             <div className={style.main_wrapper}>
@@ -134,7 +163,7 @@ function UserJoin() {
                 <input
                   type="text"
                   placeholder={"닉네임을 입력하세요"}
-                  {...register("nickname",{required: "닉네임을 입력하세요"})}
+                  {...register("nickname", { required: "닉네임을 입력하세요" })}
                 />
                 <button>Enter</button>
               </div>
