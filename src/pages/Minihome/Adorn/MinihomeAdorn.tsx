b31a9b495dc8bf747f15ed16f74c29672fb00467
holajm <holajjm@naver.com>
2025-02-28 17:03:38 +0900

    Feat: 아이템 및 배경 요소 꾸미기 기능 임시 구현

--- src/pages/Minihome/Adorn/MinihomeAdorn.tsx
+++ src/pages/Minihome/Adorn/MinihomeAdorn.tsx
@@ -1,18 +1,47 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import style from "@styles/Minihome/Adorn/MinihomeAdorn.module.css";
 import MinihomeAdornBackground from "./MinihomeAdornBackground";
 import MinihomeAdornItemList from "./MinihomeAdornItemList";
 import Button from "@components/Button";
 import { SlArrowLeft } from "react-icons/sl";
 import usePageTitle from "@hooks/usePageTitle";
+import useImage from "@hooks/useImage";
+
+import Draggable from "react-draggable";
+// import MinihomeAdornDraggableItem from "./MinihomeAdornDraggableItem";
+
+interface ItemData {
+  imageUrl: string;
+  itemCnt: number;
+  itemGrade: string;
+  itemId: number;
+  itemName: string;
+  userItemIds: null;
+}
+interface BackgroundItemData {
+  backgroundId: number;
+  imageUrl: string;
+}
 
 function MinihomeAdorn() {
-  usePageTitle("미니홈 꾸미기")
+  usePageTitle("미니홈 꾸미기");
   const [active, setActive] = useState<string | null>("BACKGROUND");
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     setActive((e.target as HTMLElement).getAttribute("datatype"));
   };
-
+  // ----------------------------------------
+  const [item, setItem] = useState<ItemData>();
+  const handleItem = (data:ItemData) => {
+    setItem(data);
+  };
+  const [background, setBackground] = useState<BackgroundItemData>();
+  const handleBackground = (data: BackgroundItemData) => {
+    setBackground(data);
+  };
+  const nodeRef = useRef(null);
+  // const draggableItems = item.map((e) => (
+  //   <MinihomeAdornDraggableItem key={e.itemId} data={e} />
+  // ));
   return (
     <div className={style.container}>
       <div className={style.wrapper}>
@@ -30,7 +59,27 @@ function MinihomeAdorn() {
           <button className={style.aside_save}>저장하기</button>
         </aside>
         <section className={style.section}>
-          <header className={style.header}>꾸민 모습</header>
+          <header className={style.header}>
+            <img
+              className={style.header_background}
+              src={useImage(background?.imageUrl as string)}
+              alt=""
+            />
+            {/* {draggableItems} */}
+            <Draggable bounds="parent" nodeRef={nodeRef}>
+              <div ref={nodeRef}>
+                <img
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "contain",
+                  }}
+                  src={useImage(item?.imageUrl as string)}
+                  alt=""
+                />
+              </div>
+            </Draggable>
+          </header>
           <main className={style.main}>
             <nav onClick={handleClick} className={style.main_nav}>
               <button
@@ -51,9 +100,9 @@ function MinihomeAdorn() {
               </button>
             </nav>
             {active === "BACKGROUND" || !active ? (
-              <MinihomeAdornBackground />
+              <MinihomeAdornBackground getBack={handleBackground} />
             ) : (
-              <MinihomeAdornItemList />
+              <MinihomeAdornItemList getItem={handleItem} />
             )}
           </main>
         </section>
