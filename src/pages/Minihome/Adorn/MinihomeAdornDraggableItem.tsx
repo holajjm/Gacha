d8eab0ce06f1b024cacdf3eacb4b8144b93cb767
holajm <holajjm@naver.com>
2025-02-28 17:05:54 +0900

    Feat: 드래그 가능 아이템 요소 컴포넌트 분리 시도

---
+++ src/pages/Minihome/Adorn/MinihomeAdornDraggableItem.tsx
@@ -0,0 +1,35 @@
+import useImage from "@hooks/useImage";
+import React, { useRef } from "react";
+import Draggable from "react-draggable";
+
+interface ItemData {
+  imageUrl: string;
+  itemCnt: number;
+  itemGrade: string;
+  itemId: number;
+  itemName: string;
+  userItemIds: null;
+}
+
+function MinihomeAdornDraggableItem({data}:{data:ItemData}) {
+  const nodeRef = useRef(null);
+  console.log(data);
+  
+  return (
+    <Draggable bounds="parent" nodeRef={nodeRef}>
+      <div ref={nodeRef}>
+        <img
+          style={{
+            width: "100%",
+            height: "100%",
+            objectFit: "contain",
+          }}
+          src={useImage(data?.imageUrl as string)}
+          alt=""
+        />
+      </div>
+    </Draggable>
+  );
+}
+
+export default MinihomeAdornDraggableItem;
