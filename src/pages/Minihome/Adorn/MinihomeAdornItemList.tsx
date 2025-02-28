f66a10148c6edd1638cf2df12311ae242e0fe511
holajm <holajjm@naver.com>
2025-02-28 17:07:32 +0900

    Feat: 하위 컴포넌트에 아이템 전달 위한 props shift 로직 구현

--- src/pages/Minihome/Adorn/MinihomeAdornItemList.tsx
+++ src/pages/Minihome/Adorn/MinihomeAdornItemList.tsx
@@ -7,37 +7,39 @@ interface ItemData {
   imageUrl: string;
   itemCnt: number;
   itemGrade: string;
-  itemId: number,
-  itemName: string,
-  userItemIds: null,
+  itemId: number;
+  itemName: string;
+  userItemIds: null;
 }
 
-function MinihomeAdornItemList() {
+function MinihomeAdornItemList({getItem}:{getItem:(data:ItemData) => void}) {
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
   const [itemList, setItemList] = useState<ItemData[]>([]);
   const getItems = async () => {
-    const response = await fetch(`${SERVER_API}/items/me/forSale`,{
+    const response = await fetch(`${SERVER_API}/items/me/forSale`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${user?.accessToken}`
-      }
-    })
+        Authorization: `Bearer ${user?.accessToken}`,
+      },
+    });
     const data = await response.json();
-    console.log(data?.data?.content);
-    
     setItemList(data?.data?.content);
-  }
+  };
   useEffect(() => {
-    getItems()
-  },[])
-  const items = itemList.map(e => <MinihomeAdornItem key={e.itemId} data={e}/>)
-  return (
-    <section className={style.main}>
-      {items}
-    </section>
-  );
+    getItems();
+  }, []);
+  // -------------------------------------
+  // const [clickItem, setClickItem] = useState<ItemData>();
+  const handleClickItem = (data:ItemData) => {
+    getItem(data);
+  }
+  // ------------------------------------
+  const items = itemList.map((e) => (
+    <MinihomeAdornItem key={e.itemId} data={e} onClick={handleClickItem}/>
+  ));
+  return <section className={style.main}>{items}</section>;
 }
 
 export default MinihomeAdornItemList;
