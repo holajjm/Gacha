2f37be30151e8c69a4cdbf75f5d72f59a48decd1
holajm <holajjm@naver.com>
2025-03-06 18:25:02 +0900

    Style: 코드 정리

--- src/pages/Explore/ExploreItem.tsx
+++ src/pages/Explore/ExploreItem.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-
-import style from "@styles/Explore/ExploreItem.module.css";
 import { useNavigate } from "react-router-dom";
+
 import useImage from "@hooks/useImage";
 import Button from "@components/Button";
 
+import style from "@styles/Explore/ExploreItem.module.css";
+
 interface ExploreItemData {
-  profileImageStoreFileName: string,
-  nickname: string,
-  totalVisitorCnt: number
+  profileImageStoreFileName: string;
+  nickname: string;
+  totalVisitorCnt: number;
 }
 
-function ExploreItem({data}:{data:ExploreItemData}) {
+function ExploreItem({ data }: { data: ExploreItemData }) {
   const navigate = useNavigate();
   return (
     <li className={style.main_list}>
@@ -22,7 +23,11 @@ function ExploreItem({data}:{data:ExploreItemData}) {
       <div>{data?.nickname}</div>
       <div>{data?.totalVisitorCnt}</div>
       <div>
-        <Button text={"방문"} width={"6rem"} onClick={() => navigate(`/minihome/${data?.nickname}`)}></Button>
+        <Button
+          text={"방문"}
+          width={"6rem"}
+          onClick={() => navigate(`/minihome/${data?.nickname}`)}
+        ></Button>
       </div>
     </li>
   );
