882e40ce7e0ff33d42ef151a5834c33a3f4fc078
holajm <holajjm@naver.com>
2025-03-06 18:13:30 +0900

    Feat: 팔로워 삭제 기능 구현 & Refactor: 버튼 공용 컴포넌트 사용

--- src/pages/Minihome/Header/MinihomeFollowerItem.tsx
+++ src/pages/Minihome/Header/MinihomeFollowerItem.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import { useUserStore } from "@store/store";
+import useImage from "@hooks/useImage";
+import Button from "@components/Button";
 
 import style from "@styles/Minihome/Header/MinihomeFollowerItem.module.css";
-import useImage from "@hooks/useImage";
 
 interface Followers {
   userId: number;
@@ -12,16 +16,41 @@ interface Followers {
   isCurrentUser: boolean;
 }
 
-function MiniHomeFollowerItem({followers}:{followers:Followers}) {
-  console.log(followers);
-  
+function MiniHomeFollowerItem({ followers }: { followers: Followers }) {
+  const SERVER_API = import.meta.env.VITE_SERVER_API;
+  const { user } = useUserStore((state) => state);
+  const { nickname } = useParams();
+  const deleteFollower = async () => {
+    if (confirm("팔로워를 삭제할까요?")) {
+      try {
+        await fetch(`${SERVER_API}/users/follower/${followers?.nickname}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.accessToken}`,
+          },
+        });
+        alert("삭제되었습니다.");
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <li className={style.list}>
       <div>
         <img src={useImage(followers?.profileImageUrl)} alt="profile" />
       </div>
       <p>{followers?.nickname}</p>
-      <button>삭제</button>
+      {nickname === user?.nickname ? (
+        <Button
+          text={"삭제"}
+          width={"3.5rem"}
+          onClick={deleteFollower}
+        ></Button>
+      ) : null}
     </li>
   );
 }
