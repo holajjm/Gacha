6685c5c0a92df2442aaf84e0e1b75993c1600847
holajm <holajjm@naver.com>
2025-03-06 18:15:25 +0900

    Feat: 언팔로우 기능 구현 & Refactor: 버튼 공용 컴포넌트 사용

--- src/pages/Minihome/Header/MinihomeFollowingItem.tsx
+++ src/pages/Minihome/Header/MinihomeFollowingItem.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 
+import { useUserStore } from "@store/store";
 import useImage from "@hooks/useImage";
+import Button from "@components/Button";
+
 import style from "@styles/Minihome/Header/MinihomeFollowingItem.module.css";
 
 interface Followings {
@@ -11,16 +14,33 @@ interface Followings {
   isCurrentUser: boolean;
 }
 
-function MinihomeFollowingItem({followings}:{followings:Followings}) {
+function MinihomeFollowingItem({ followings }: { followings: Followings }) {
   console.log(followings);
-  
+  const SERVER_API = import.meta.env.VITE_SERVER_API;
+  const { user } = useUserStore((state) => state);
+  const unFollow = async () => {
+    if (confirm(`${followings?.nickname}님을 언팔로우 할까요?`)) {
+      await fetch(`${SERVER_API}/users/unfollow`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.accessToken}`,
+        },
+        body: JSON.stringify({
+          followeeUserNickname: followings?.nickname,
+        }),
+      });
+      alert("언팔로우 되었습니다.");
+      window.location.reload();
+    }
+  };
   return (
     <li className={style.list}>
       <div>
         <img src={useImage(followings?.profileImageUrl)} alt="profile" />
       </div>
       <p>{followings?.nickname}</p>
-      <button>삭제</button>
+      <Button text={"삭제"} width={"3.5rem"} onClick={unFollow}></Button>
     </li>
   );
 }
