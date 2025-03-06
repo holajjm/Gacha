606d4c1a27bb81720ed4cb81ee586cd965a0bad3
holajm <holajjm@naver.com>
2025-03-06 18:26:18 +0900

    Feat: 스크롤 상단 커스텀 훅 사용 및 코드 정리

--- src/pages/Gacha/GachaMain.tsx
+++ src/pages/Gacha/GachaMain.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { useUserStore } from "@store/store";
 
-import GachaCapsule from "./GachaCapsule";
-import style from "@styles/Gacha/GachaMain.module.css";
 import Coin from "@components/Coin";
 import Button from "@components/Button";
 import usePageTitle from "@hooks/usePageTitle";
+import usePageUpper from "@hooks/usePageUpper";
+
+import GachaCapsule from "./GachaCapsule";
+import style from "@styles/Gacha/GachaMain.module.css";
 
 interface GachaData {
   itemGrade: string;
@@ -14,7 +16,8 @@ interface GachaData {
 }
 
 function GachaMain() {
-  usePageTitle("뽑기")
+  usePageTitle("뽑기");
+  usePageUpper();
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
   const [open, setOpen] = useState<boolean>(false);
@@ -32,7 +35,6 @@ function GachaMain() {
       },
     });
     const data = await response.text();
-    console.log(JSON.parse(data)?.data);
     setGachaData(JSON.parse(data)?.data);
   };
   const handleGachaClick = () => {
@@ -40,8 +42,8 @@ function GachaMain() {
     setOpen(true);
   };
   const handleModalClose = () => {
-    setOpen(false)
-  }
+    setOpen(false);
+  };
   return (
     <div className={style.container}>
       <section className={style.wrapper}>
@@ -54,10 +56,16 @@ function GachaMain() {
           <div>
             <img src="/images/GachaSample.svg" alt="Gacha" />
           </div>
-          <Button text={"아이템 뽑기"} width={"10rem"} onClick={handleGachaClick}></Button>
+          <Button
+            text={"아이템 뽑기"}
+            width={"10rem"}
+            onClick={handleGachaClick}
+          ></Button>
         </main>
       </section>
-      {open ? <GachaCapsule imageData={gachaData} onClick={handleModalClose} /> : null}
+      {open ? (
+        <GachaCapsule imageData={gachaData} onClick={handleModalClose} />
+      ) : null}
     </div>
   );
 }
