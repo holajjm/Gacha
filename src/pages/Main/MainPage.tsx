b2c961906b7f2ce9b00e2d44bbd474352cd2d48b
holajm <holajjm@naver.com>
2025-03-06 18:25:32 +0900

    Feat: 스크롤 상단 커스텀 훅 사용

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import usePageUpper from "@hooks/usePageUpper";
+
 import style from "@styles/Main/MainPage.module.css";
 
 function MainPage() {
+  usePageUpper();
   return (
     <div className={style.container}>
       <section className={style.wrapper}>MainPage</section>
