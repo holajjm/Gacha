dba39f9a8914e9409aadb209e6150e5767810f25
holajm <holajjm@naver.com>
2025-03-07 14:42:24 +0900

    Style: 코드 정리

--- src/pages/Minihome/Header/MinihomeItems.tsx
+++ src/pages/Minihome/Header/MinihomeItems.tsx
@@ -1,30 +1,28 @@
 import React from "react";
 
 import useImage from "@hooks/useImage";
+
 import style from "@styles/Minihome/Header/MinihomeItem.module.css";
 
 interface ItemBookData {
   imageUrl: string;
   itemCnt: number;
   itemGrade: string;
-  itemId: number,
-  itemName: string,
-  userItemIds: null,
+  itemId: number;
+  itemName: string;
+  userItemIds: null;
 }
 
-function MinihomeItems({data}:{data:ItemBookData}) {
-  const grade = data?.itemGrade;
+function MinihomeItems({ data }: { data: ItemBookData }) {
   return (
-    <div className={style[`wrapper_${grade}`]}>
+    <div className={style[`wrapper_${data?.itemGrade}`]}>
       {!data?.itemCnt ? <div className={style.disabled}></div> : null}
       <div className={style.img}>
         <img src={useImage(data?.imageUrl)} alt="sample" />
-        {data?.itemCnt ? <p className={style.count}>{data?.itemCnt}</p> : null}
+        {data?.itemCnt ? (
+          <p className={style.count}>{data?.itemCnt}개</p>
+        ) : null}
       </div>
-      {/* <div className={style.item}>
-        <p>{data?.itemGrade}</p>
-        <p>{data?.itemName} / {data?.itemCnt}개</p>
-      </div> */}
     </div>
   );
 }
