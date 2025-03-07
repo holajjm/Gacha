e2a02316ccb3873405b6800e500e95ad66b96638
holajm <holajjm@naver.com>
2025-03-07 15:39:26 +0900

    Style: 코드 정리

--- src/pages/Market/Enroll/MarketEnrollItem.tsx
+++ src/pages/Market/Enroll/MarketEnrollItem.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 import useImage from "@hooks/useImage";
 
 import style from "@styles/Market/Enroll/MarketEnrollItem.module.css";
@@ -11,6 +12,7 @@ interface Item {
   itemName: string;
   price: number;
   stock: number;
+  userItemIds: number[];
 }
 
 function MarketEnrollItem({
@@ -23,11 +25,17 @@ function MarketEnrollItem({
   const handleClick = () => {
     onSelect(item);
   };
-  
+
   return (
     <div className={style[`item_${item?.itemGrade}`]} onClick={handleClick}>
-      <img className={style.item_img} src={useImage(item?.imageUrl)} alt="sample" />
-      {item?.itemCnt > 0 ? <p className={style.item_cnt}>{item?.itemCnt}</p> : null}
+      <img
+        className={style.item_img}
+        src={useImage(item?.imageUrl)}
+        alt="sample"
+      />
+      {item?.itemCnt > 0 ? (
+        <p className={style.item_cnt}>{item?.itemCnt}</p>
+      ) : null}
     </div>
   );
 }
