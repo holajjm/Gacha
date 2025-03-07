fdf825feb9130e739f1a330462f0695f89b3d2f0
holajm <holajjm@naver.com>
2025-03-07 15:35:38 +0900

    Refactor: 판매 상품 가격 표시 요소 생성

--- src/pages/Market/MarketItemModal.tsx
+++ src/pages/Market/MarketItemModal.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
-import style from "@styles/Market/MarketItemModal.module.css";
-import { useUserStore } from "@store/store";
+
+import { useCoinState, useUserStore } from "@store/store";
 import useImage from "@hooks/useImage";
+import Button from "@components/Button";
+
+import style from "@styles/Market/MarketItemModal.module.css";
 
 interface ModalData {
   grade: string;
   imageUrl: string;
   name: string;
   stock: number;
+  price: number;
 }
 
 function MarketItemModal({
@@ -23,6 +27,7 @@ function MarketItemModal({
     imageUrl: "",
     name: "",
     stock: 0,
+    price: 0,
   });
   const { user } = useUserStore((state) => state);
   const getModalData = async () => {
@@ -38,12 +43,33 @@ function MarketItemModal({
     );
     const data = await response.json();
     console.log(data?.data);
-    
+
     setModalData(data?.data);
   };
   useEffect(() => {
     getModalData();
   }, []);
+
+  // 재고가 없을 경우 구매 불가 예외 처리 로직 구현 요망
+  const triggerRefresh = useCoinState((state) => state.triggerRefresh);
+  const buyItem = async () => {
+    if (confirm("구매하시겠습니까?")) {
+      try {
+        await fetch(`${SERVER_API}/items/${clickItemId}/purchase`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.accessToken}`,
+          },
+        });
+        alert("구매 성공");
+        triggerRefresh();
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
 
   return (
     <div className={style.container}>
@@ -58,12 +84,23 @@ function MarketItemModal({
           </aside>
           <main className={style.main}>
             <div className={style.main_wrapper}>
-              <p>이름 <span>{modalData?.name}</span></p>
-              <p>등급 <span>{modalData?.grade}</span></p>
-              <p>수량 <span>{modalData?.stock}</span></p>
+              <p>
+                이름 <span>{modalData?.name}</span>
+              </p>
+              <p>
+                등급 <span>{modalData?.grade}</span>
+              </p>
+              <p>
+                가격 <span>{modalData?.price}</span>
+              </p>
+              <p>
+                수량 <span>{modalData?.stock}</span>
+              </p>
             </div>
             {/* 구매 기능 구현 및 공용 버튼 컴포넌트로 변경 */}
-            <button className={style.main_button}>구매</button>
+            <footer className={style.main_footer}>
+              <Button text={"구매"} width={"5rem"} onClick={buyItem}></Button>
+            </footer>
           </main>
         </section>
       </section>
