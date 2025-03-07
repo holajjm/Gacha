96975c6b5085587e6f2e0bdc23b0dc5aa479cdbf
holajm <holajjm@naver.com>
2025-03-07 14:36:58 +0900

    Style: 코드 정리

--- src/pages/Minihome/Adorn/MinihomeAdornItemList.tsx
+++ src/pages/Minihome/Adorn/MinihomeAdornItemList.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-import style from "@styles/Minihome/Adorn/MinihomeAdornItemList.module.css";
+
 import { useUserStore } from "@store/store";
+
 import MinihomeAdornItem from "./MinihomeAdornItem";
+import style from "@styles/Minihome/Adorn/MinihomeAdornItemList.module.css";
 
 interface ItemData {
   imageUrl: string;
@@ -12,12 +14,16 @@ interface ItemData {
   userItemIds: null;
 }
 
-function MinihomeAdornItemList({getItem}:{getItem:(data:ItemData) => void}) {
+function MinihomeAdornItemList({
+  getItem,
+}: {
+  getItem: (data: ItemData) => void;
+}) {
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
   const [itemList, setItemList] = useState<ItemData[]>([]);
   const getItems = async () => {
-    const response = await fetch(`${SERVER_API}/items/me/forSale`, {
+    const response = await fetch(`${SERVER_API}/items/me`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
@@ -30,14 +36,11 @@ function MinihomeAdornItemList({getItem}:{getItem:(data:ItemData) => void}) {
   useEffect(() => {
     getItems();
   }, []);
-  // -------------------------------------
-  // const [clickItem, setClickItem] = useState<ItemData>();
-  const handleClickItem = (data:ItemData) => {
+  const handleClickItem = (data: ItemData) => {
     getItem(data);
-  }
-  // ------------------------------------
+  };
   const items = itemList.map((e) => (
-    <MinihomeAdornItem key={e.itemId} data={e} onClick={handleClickItem}/>
+    <MinihomeAdornItem key={e.itemId} data={e} onClick={handleClickItem} />
   ));
   return <section className={style.main}>{items}</section>;
 }
