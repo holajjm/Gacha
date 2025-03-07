0e34a24e86d4d89d6d3f1d5a7467fa9e3e37433d
holajm <holajjm@naver.com>
2025-03-07 16:06:05 +0900

    Refactor: 모달 분리에 따른 데이터 props 전달 받기 및 아이템 판매 취소 로직 구현, 버튼 공용 컴포넌트 사용

--- src/pages/Market/Sell/MarketSellingItemModal.tsx
+++ src/pages/Market/Sell/MarketSellingItemModal.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect, useState } from "react";
-import style from "@styles/Market/Sell/MarketSellingItemModal.module.css";
+import React from "react";
+
 import { useUserStore } from "@store/store";
 import useImage from "@hooks/useImage";
+import Button from "@components/Button";
+
+import style from "@styles/Market/Sell/MarketSellingItemModal.module.css";
 
 interface MySellingItemData {
   grade: string;
@@ -13,45 +16,33 @@ interface MySellingItemData {
   transactionDate: string | null;
 }
 
-function MarketSellingItemModal({ clickedItemId,onClick }: { clickedItemId:number,onClick: () => void }) {
+function MarketSellingItemModal({
+  onClick,
+  data,
+}: {
+  onClick: () => void;
+  data: MySellingItemData;
+}) {
   const SERVER_API = import.meta.env.VITE_SERVER_API;
-  const [modalData, setModalData] = useState<MySellingItemData[]>([]);
   const { user } = useUserStore((state) => state);
-  const getModalData = async () => {
-    const response = await fetch(`${SERVER_API}/products/me`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user?.accessToken}`,
-      },
-    });
-    const data = await response.json();
-    setModalData(data?.content);
+  const handleCancel = async () => {
+    if (confirm("판매 취소 하시겠습니까?")) {
+      try {
+        await fetch(`${SERVER_API}/products/${data?.productId}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${user?.accessToken}`,
+          },
+        });
+        alert("취소 되었습니다.");
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+      }
+    }
   };
-  useEffect(() => {
-    getModalData();
-  }, []);
 
-  // const [imageList, setImageList] = useState<string>();
-  // const image = async () => {
-  //   if(modalData[clickedItemId-1]){
-  //     const response = await fetch(
-  //       `${SERVER_API}${modalData[clickedItemId-1]?.imageUrl}`,
-  //       {
-  //         method: "GET",
-  //         headers: {
-  //           "Content-Type": "image/png, image/jif",
-  //         },
-  //       },
-  //     );
-  //     const blob = await response.blob();
-  //     const imageObjUrl = URL.createObjectURL(blob);
-  //     setImageList(imageObjUrl);
-  //   }
-  // };
-  // useEffect(() => {
-  //   image();
-  // }, [modalData]);
   return (
     <div className={style.container}>
       <section className={style.wrapper}>
@@ -61,16 +52,22 @@ function MarketSellingItemModal({ clickedItemId,onClick }: { clickedItemId:numbe
         </button>
         <section className={style.contents}>
           <aside className={style.aside}>
-            <img src={useImage(modalData[clickedItemId-1]?.imageUrl)} alt="sample" />
+            <img src={useImage(data?.imageUrl)} alt="sample" />
           </aside>
           <main className={style.main}>
-            <p>이름 | {modalData[clickedItemId-1]?.name}</p>
-            <p>등급 | {modalData[clickedItemId-1]?.grade}</p>
-            <p>판매상태 | {modalData[clickedItemId-1]?.status}</p>
-            {modalData[clickedItemId-1]?.status !== "판매 중" ? (
-              <p>거래일자 | {modalData[clickedItemId-1]?.transactionDate}</p>
+            <p>이름 | {data?.name}</p>
+            <p>등급 | {data?.grade}</p>
+            <p>판매상태 | {data?.status}</p>
+            {data?.status !== "판매 중" ? (
+              <p>거래일자 | {data?.transactionDate}</p>
             ) : null}
-            <button>판매 취소</button>
+            <div>
+              <Button
+                text={"판매 취소"}
+                width={"5rem"}
+                onClick={handleCancel}
+              ></Button>
+            </div>
           </main>
         </section>
       </section>
