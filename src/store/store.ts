7976947167e180bbfe124991aba212470d313dd9
holajm <holajjm@naver.com>
2025-03-07 16:06:53 +0900

    Feat: 공용 컴포넌트 리렌더링에 따른 전역 상태 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 
 export interface User {
-  // isNewUser: string;
   loginId: string;
   socialType: string;
   nickname: string;
@@ -19,4 +18,15 @@ export const useUserStore = create<UserStore>((set) => ({
     sessionStorage.setItem("user", JSON.stringify(user));
     set({ user });
   },
+}));
+
+interface CoinState {
+  refreshTrigger: number;
+  triggerRefresh: () => void;
+}
+
+export const useCoinState = create<CoinState>((set) => ({
+  refreshTrigger: 0,
+  triggerRefresh: () =>
+    set((state) => ({ refreshTrigger: state.refreshTrigger + 1 })),
 }));
