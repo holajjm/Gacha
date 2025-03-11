242b009e546218995cebd11b55e068d0f522c440
holajm <holajjm@naver.com>
2025-03-11 16:43:17 +0900

    Feat: 신규 프로필 이미지 데이터 파일 생성

---
+++ src/assets/Profile.ts
@@ -0,0 +1,16 @@
+const ProfileImg = [
+  {
+    id: 1,
+    profileImg: "/images/Bear.svg",
+  },
+  {
+    id: 2,
+    profileImg: "/images/Cow.svg",
+  },
+  {
+    id: 3,
+    profileImg: "/images/Giraffe.svg",
+  },
+];
+
+export default ProfileImg;
