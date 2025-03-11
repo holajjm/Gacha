648da496d025ff8295aff130550d422386f3d91a
holajm <holajjm@naver.com>
2025-03-11 16:43:51 +0900

    Fix: 전역 상태 속성 수정

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,7 +4,7 @@ export interface User {
   loginId: string;
   socialType: string;
   nickname: string;
-  profileUrl: string;
+  profileId: string;
   accessToken: string;
   refreshToken: string;
 }
