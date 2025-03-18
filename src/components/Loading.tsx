8c4997bfebdac8d8ff930ac707e298482d911f0b
holajm <holajjm@naver.com>
2025-03-18 15:30:17 +0900

    Refactor: 로딩 UI 이미지 추가

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import usePageUpper from "@hooks/usePageUpper";
+
+import style from "@styles/Layouts/Loading.module.css";
+
 function Loading() {
-  return <div>Loading</div>;
+  usePageUpper();
+  return (
+    <div className={style.container}>
+      <section className={style.wrapper}>
+        <header className={style.header}>
+          <h1 className={style.title}>
+            <img src="/images/Loading.gif" alt="loading" />
+          </h1>
+        </header>
+      </section>
+    </div>
+  );
 }
 
 export default Loading;
