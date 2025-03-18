1481d941a73990124bb231a53bbb828a00324a9e
holajm <holajjm@naver.com>
2025-03-18 15:29:40 +0900

    Refactor: 사용자의 동작에 따른 전역상태를 활용한 코인 동시 리렌더링 로직 개선 및 요청 헤더값 정리

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -10,21 +10,21 @@ import style from "@styles/Layouts/Coin.module.css";
 function Coin() {
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
-  const coinUpDate = useCoinState((state) => state.coinRefresh);
+  const { coinUpdate } = useCoinState((state) => state);
   const getCoin = async () => {
     const response = await axios.get(`${SERVER_API}/coin`, {
       headers: {
-        "Content-Type": "application/json",
         Authorization: `Bearer ${user?.accessToken}`,
       },
     });
     return response;
   };
   const { data, isLoading } = useQuery({
-    queryKey: ["coin", coinUpDate],
+    queryKey: ["coin", coinUpdate],
     queryFn: getCoin,
     select: (data) => data?.data?.data,
   });
+
   return (
     <div className={style.coin}>
       <img src="/images/NewCoin.svg" alt="coin" />
