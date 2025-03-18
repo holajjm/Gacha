9350aa17a2948ae1585e4907c1540f22eb5944d2
holajm <holajjm@naver.com>
2025-03-18 15:28:31 +0900

    Feat: Suspense 및 fallback UI 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+
+import Loading from "@components/Loading";
+
 import "./App.css";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
 
 export default App;
