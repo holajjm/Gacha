e1276000aec7d6c1a944111c354ba9504060da4d
holajm <holajjm@naver.com>
2025-03-18 19:50:04 +0900

    Feat: React Query 적용을 위한 QueryClient 및 Provider 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React, { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./routes";
 
 import Loading from "@components/Loading";
 
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Suspense fallback={<Loading />}>
-      <RouterProvider router={router} />
-    </Suspense>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<Loading />}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </QueryClientProvider>
   );
 }
 
