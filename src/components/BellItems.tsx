b35904a403ca246335ee72d8d97c15d96b7d2619
holajm <holajjm@naver.com>
2025-03-27 17:59:26 +0900

    Feat: 세부 알림 목록 컴포넌트 분리

---
+++ src/components/BellItems.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+
+import style from "@styles/Layouts/BellItems.module.css";
+
+interface NotiDetailData {
+  itemGrade?: string;
+  itemName?: string;
+  coin?: number;
+}
+interface Notis {
+  id: number;
+  type: string;
+  data: NotiDetailData;
+  read: boolean;
+}
+interface NotiData {
+  count: number;
+  hasNewNotifications: boolean;
+  notifications: Notis[];
+}
+
+function BellItem({ data }: { data: NotiDetailData }) {
+  return (
+    <article className={style.article}>
+      {data?.itemGrade ? (
+        <p>
+          {data?.itemGrade}등급의 아이템을 모두 모아 복권이 지급되었습니다.
+          여기를 눌러 복권 결과를 확인해보세요!
+        </p>
+      ) : (
+        <p>
+          마켓에 등록한 {data?.itemName}가 판매되어 {data?.coin} 코인이
+          지급되었습니다
+        </p>
+      )}
+    </article>
+  );
+}
+
+function BellItems({ data }: { data: NotiData }) {
+  console.log(data);
+  const [notis, setNotis] = useState<Notis[]>([]);
+  useEffect(() => {
+    setNotis(data?.notifications);
+  }, [data]);
+  const itemList = notis.map((e) => <BellItem key={e?.id} data={e?.data} />);
+
+  return (
+    <section className={style.container}>
+      {data?.count ? itemList : <span>알림이 없습니다.</span>}
+    </section>
+  );
+}
+
+export default BellItems;
