5e8ee4ddf1c877d9253537d9e71add3a37467003
holajm <holajjm@naver.com>
2025-03-27 17:59:09 +0900

    Feat: 알림 기능 컴포넌트 추가

--- src/components/Bell.tsx
+++ src/components/Bell.tsx
@@ -1,38 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 import { useUserStore } from "@store/store";
 
+import BellItems from "./BellItems";
 import style from "@styles/Layouts/Bell.module.css";
 
+interface NotiDetailData {
+  itemGrade?: string;
+  itemName?: string;
+  coin?: number;
+}
+interface NotiData {
+  count: number;
+  hasNewNotifications: boolean;
+  notifications: {
+    id: number;
+    type: string;
+    data: NotiDetailData;
+    read: boolean;
+  }[];
+}
 function Bell() {
+  const [click, setClick] = useState(false);
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
+  const [noti, setNoti] = useState<NotiData>({
+    count: 0,
+    hasNewNotifications: false,
+    notifications: [
+      {
+        id: 0,
+        type: "LOTTO_ISSUED",
+        data: {
+          itemGrade: "",
+          itemName: "",
+          coin: 0,
+        },
+        read: false,
+      },
+    ],
+  });
   const getNoti = async () => {
     const response = await axios.get(`${SERVER_API}/notifications`, {
       headers: {
         Authorization: `Bearer ${user?.accessToken}`,
       },
     });
-    // const data = await response.json();
-    // console.log(response?.data?.data);
     return response;
   };
   const { data } = useQuery({
     queryKey: ["Noti"],
     queryFn: getNoti,
-    select: (data) => data?.data?.data
+    select: (data) => data?.data?.data,
   });
-  console.log(data);
-  
-  // useEffect(() => {
-  //   getNoti();
-  // }, []);
+  useEffect(() => {
+    setNoti(data);
+  }, [data]);
+
   return (
-    <div className={style.container}>
-      <img src="/src/assets/images/Bell.svg" alt="Bell" />
-    </div>
+    <aside onClick={() => setClick(!click)} className={style.container}>
+      <img src="/images/Bell.svg" alt="Bell" />
+      {data?.count ? <span className={style.isItem}></span> : null}
+      {click ? <BellItems data={noti} /> : null}
+    </aside>
   );
 }
 
