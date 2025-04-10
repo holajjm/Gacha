6c7f15278885d8505dcb53c4027786fc08b47c4f
holajm <holajjm@naver.com>
2025-04-10 20:03:30 +0900

    Feat: 모달 창 여닫힘 상태 전역 상태 추가

--- src/store/store.ts
+++ src/store/store.ts
@@ -29,3 +29,15 @@ export const useCoinState = create<CoinState>((set) => ({
   coinUpdate: 0,
   coinRefresh: () => set((state) => ({ coinUpdate: state.coinUpdate + 1 })),
 }));
+
+interface ModalState {
+  modal: boolean;
+  modalClose: () => void;
+  modalOpen: () => void;
+}
+
+export const useModalState = create<ModalState>((set) => ({
+  modal: false,
+  modalClose: () => set({ modal: false }),
+  modalOpen: () => set({ modal: true }),
+}));
