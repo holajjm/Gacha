dfab2316b675741231e74662e2d6b02ee9f6fb0e
holajm <holajjm@naver.com>
2025-04-10 20:05:19 +0900

    Refactor: 이미지 태그 LCP 개선

--- src/pages/Minihome/ItemBook/MinihomeItems.tsx
+++ src/pages/Minihome/ItemBook/MinihomeItems.tsx
@@ -18,7 +18,15 @@ function MinihomeItems({ data }: { data: ItemBookData }) {
     <article className={style[`article_item_${data?.itemGrade}`]}>
       {!data?.itemCnt ? <p className={style.article_item_disabled}></p> : null}
       <div className={style.article_item_img}>
-        <img src={useImage(data?.imageUrl)} alt="sample" />
+        <img
+          src={useImage(data?.imageUrl)}
+          alt="sample"
+          width={96}
+          height={96}
+          loading="lazy"
+          decoding="async"
+          fetchPriority="high"
+        />
         {data?.itemCnt ? (
           <p className={style.article_item_count}>{data?.itemCnt}개</p>
         ) : null}
