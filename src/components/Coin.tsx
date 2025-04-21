e00b73225e7545886449751070ba493fe04fa103
holajm <holajjm@naver.com>
2025-04-21 17:59:55 +0900

    Refactor: 데이터 호출 로직 axios + RQ로 수정

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,34 +1,34 @@
 import React from "react";
-import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
 
 import { useCoinState, useUserStore } from "@store/store";
 
 import Loading from "./Loading";
 import style from "@styles/Layouts/Coin.module.css";
+import useCustomAxios from "@hooks/useCustomAxios";
 
 function Coin() {
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
   const { coinUpdate } = useCoinState((state) => state);
+  const axios = useCustomAxios();
   const getCoin = async () => {
-    const response = await axios.get(`${SERVER_API}/coin`, {
-      headers: {
-        Authorization: `Bearer ${user?.accessToken}`,
-      },
-    });
-    return response;
+    const response = await axios.get(`${SERVER_API}/coin`);
+    // console.log(response?.data);
+    return response?.data;
   };
   const { data, isLoading } = useQuery({
-    queryKey: ["coin", coinUpdate],
+    queryKey: ["coin", coinUpdate, user],
     queryFn: getCoin,
-    select: (data) => data?.data?.data,
+    select: data => data?.coin,
+    refetchInterval: 1000 * 60
   });
+// console.log(data);
 
   return (
     <div className={style.coin}>
       <img src="/images/NewCoin.svg" alt="coin" />
-      {isLoading ? <Loading /> : <p>{data?.coin}</p>}
+      {isLoading ? <Loading /> : <p>{data}</p>}
     </div>
   );
 }
