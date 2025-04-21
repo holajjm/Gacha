36791dc7bcb4e5411c885e30452e98c02e893cab
holajm <holajjm@naver.com>
2025-04-21 17:52:05 +0900

    Feat: 메인 페이지 스크롤 기반 페이지 구현

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,106 +1,77 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import usePageTitle from "@hooks/usePageTitle";
 import usePageUpper from "@hooks/usePageUpper";
-// import { motion, useScroll, useTransform } from "framer-motion";
 
-// import MainTitle from "./MainTitle";
-// import MainMiniHomePreview from "./MainMiniHomePreview";
-// import MainExplorePreview from "./MainExplorePreview";
-// import MainGachaPreview from "./MainGachaPreview";
-// import MainMarketPreview from "./MainMarketPreview";
+import MainTitle from "./MainTitle";
+import MainMiniHomePreview from "./MainMiniHomePreview";
+import MainExplorePreview from "./MainExplorePreview";
+import MainGachaPreview from "./MainGachaPreview";
+import MainMarketPreview from "./MainMarketPreview";
 import style from "@styles/Main/MainPage.module.css";
 
-// import useCustomAxios from "@hooks/useCustomAxios";
+import { useUserStore } from "@store/store";
 
 function MainPage() {
   usePageTitle("GachaGacha");
   usePageUpper();
-  // const containerRef = useRef(null);
-  // const { scrollYProgress } = useScroll({ container: containerRef });
 
-  // const opacity = useTransform(scrollYProgress, [0, 0.2], [0, 1]);
-  // const opacity1 = useTransform(scrollYProgress, [0.2, 0.4], [0, 1]);
-  // const opacity2 = useTransform(scrollYProgress, [0.4, 0.6], [0, 1]);
-  // const opacity3 = useTransform(scrollYProgress, [0.6, 0.8], [0, 1]);
-  // const opacity4 = useTransform(scrollYProgress, [0.8, 1], [0, 1]);
+  const article1Ref = useRef<HTMLDivElement>(null);
+  const article2Ref = useRef<HTMLDivElement>(null);
+  const article3Ref = useRef<HTMLDivElement>(null);
+  const article4Ref = useRef<HTMLDivElement>(null);
+  const article5Ref = useRef<HTMLDivElement>(null);
+  // 각 섹션으로 스크롤하는 함수
+  const scrollToSection = (section: React.RefObject<HTMLDivElement>) => {
+    section.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
-  // const scale = useTransform(scrollYProgress, [0, 0.2], [0.2, 1]);
-  // const scale1 = useTransform(scrollYProgress, [0.2, 0.4], [0.4, 1]);
-  // const scale2 = useTransform(scrollYProgress, [0.4, 0.6], [0.6, 1]);
-  // const scale3 = useTransform(scrollYProgress, [0.6, 0.8], [0.8, 1]);
-  // const scale4 = useTransform(scrollYProgress, [0.8, 1], [0.8, 1]);
-
-  // const SERVER_API = import.meta.env.VITE_SERVER_API;
-  // const axios = useCustomAxios();
-  // const getcoin = async () => {
-  //   const response = await axios.get(`${SERVER_API}/coin`);
-  //   // console.log(response);
-  // };
-  // useEffect(() => {
-  //   getcoin();
-  // }, []);
+  const SERVER_API = import.meta.env.VITE_SERVER_API;
+  const { user, setUser } = useUserStore((state) => state);
+  console.log("user", user);
+  const getUserInfo = async () => {
+    const response = await fetch(`${SERVER_API}/user_info`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${user?.accessToken}`,
+      },
+    });
+    const data = await response.json();
+    console.log(data?.data);
+    if (data?.data) {
+      setUser({
+        ...user,
+        nickname: data?.data?.nickname,
+        profileId: data?.data?.profileId,
+      });
+    } else if (data?.error) {
+      console.log(data?.error);
+    }
+  };
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+  useEffect(() => {
+    sessionStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
   return (
     <div className={style.container}>
+      <nav className={style.nav}>
+        <ul>
+          <li onClick={() => scrollToSection(article1Ref)}></li>
+          <li onClick={() => scrollToSection(article2Ref)}></li>
+          <li onClick={() => scrollToSection(article3Ref)}></li>
+          <li onClick={() => scrollToSection(article4Ref)}></li>
+          <li onClick={() => scrollToSection(article5Ref)}></li>
+        </ul>
+      </nav>
       <section className={style.wrapper}>
-        <section className={style.section}>
-          {/* <motion.section style={{ opacity, scale }}>
-            <MainTitle />
-          </motion.section>
-          <motion.section style={{ opacity:opacity1, scale:scale1 }}>
-            <MainMiniHomePreview />
-          </motion.section>
-          <motion.section style={{ opacity: opacity2, scale: scale2 }}>
-            <MainExplorePreview />
-          </motion.section>
-          <motion.section style={{ opacity: opacity3, scale: scale3 }}>
-            <MainGachaPreview />
-          </motion.section>
-          <motion.section style={{ opacity: opacity4, scale: scale4 }}>
-            <MainMarketPreview />
-          </motion.section> */}
-          <article className={style.header}>
-            <h1 className={style.title}>
-              Welcome to the
-              <br />
-              GACHAGACHA!
-            </h1>
-          </article>
-          <article className={style.article}>
-            <h1>MiniHome</h1>
-            <div className={style.img_preview}>
-              <img src="/images/MiniHomeMain.svg" alt="" />
-              <img src="/images/MiniHomeMain1.svg" alt="" />
-              <img src="/images/MiniHomeItemBook.svg" alt="" />
-              <img src="/images/MiniHomeAdornBackground.svg" alt="" />
-              <img src="/images/MiniHomeAdornItem.svg" alt="" />
-            </div>
-          </article>
-          <article className={style.article}>
-            <h1>Explore</h1>
-            <div className={style.img_preview}>
-              <img src="/images/Explore.svg" alt="" />
-            </div>
-          </article>
-          <article className={style.article}>
-            <h1>Gacha</h1>
-            <div className={style.img_preview}>
-              <img src="/images/Gacha.svg" alt="" />
-              <img src="/images/GachaClose.svg" alt="" />
-              <img src="/images/GachaOpen.svg" alt="" />
-            </div>
-          </article>
-          <article className={style.article}>
-            <h1>Market</h1>
-            <div className={style.img_preview}>
-              <img src="/images/Market.svg" alt="" />
-              <img src="/images/MarketItemModal.svg" alt="" />
-              <img src="/images/MarketSell.svg" alt="" />
-              <img src="/images/MarketSellModal.svg" alt="" />
-              <img src="/images/MarketEnroll.svg" alt="" />
-            </div>
-          </article>
-        </section>
+        <MainTitle article1Ref={article1Ref} />
+        <MainMiniHomePreview article2Ref={article2Ref} />
+        <MainExplorePreview article3Ref={article3Ref} />
+        <MainGachaPreview article4Ref={article4Ref} />
+        <MainMarketPreview article5Ref={article5Ref} />
       </section>
     </div>
   );
