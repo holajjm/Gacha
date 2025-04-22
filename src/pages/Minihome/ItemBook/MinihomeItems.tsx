148bec506f58991d228dc66a09a259613a0046c7
holajm <holajjm@naver.com>
2025-04-22 13:38:05 +0900

    Feat: 이미지 렌더링 최적화 적용

--- src/pages/Minihome/ItemBook/MinihomeItems.tsx
+++ src/pages/Minihome/ItemBook/MinihomeItems.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import useImage from "@hooks/useImage";
 
@@ -14,6 +14,12 @@ interface ItemBookData {
 }
 
 function MinihomeItems({ data }: { data: ItemBookData }) {
+  const imgRef = useRef<HTMLImageElement>(null);
+  useEffect(() => {
+    if (imgRef.current) {
+      imgRef.current.setAttribute("fetchpriority", "high");
+    }
+  }, []);
   return (
     <article className={style[`article_item_${data?.itemGrade}`]}>
       {!data?.itemCnt ? <p className={style.article_item_disabled}></p> : null}
@@ -21,11 +27,9 @@ function MinihomeItems({ data }: { data: ItemBookData }) {
         <img
           src={useImage(data?.imageUrl)}
           alt="sample"
+          ref={imgRef}
           width={96}
           height={96}
-          loading="lazy"
-          decoding="async"
-          fetchPriority="high"
         />
         {data?.itemCnt ? (
           <p className={style.article_item_count}>{data?.itemCnt}개</p>
