6872c9e937d673f57c8421a396647a0556f7b85a
holajm <holajjm@naver.com>
2025-04-22 13:37:19 +0900

    Feat: 아이템 북 내 아이템들 스켈레톤 UI 적용

---
+++ src/components/skeleton/MinihomeItemSkeleton.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+import style from "@styles/Layouts/skeleton/MinihomeItemSkeleton.module.css";
+
+function MinihomeItemSkeleton() {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+      <div className={style.item}></div>
+    </div>
+  );
+}
+
+export default MinihomeItemSkeleton;
