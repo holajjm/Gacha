9e59cd8ca4fc47aa1c2e921eb14d0803b3fffc9e
holajm <holajjm@naver.com>
2025-04-23 14:56:26 +0900

    Fix: RQ Provider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import React, { Suspense } from "react";
 import { RouterProvider } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "./routes";
 
 import Loading from "@components/Loading";
 
 import "./App.css";
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Suspense fallback={<Loading />}>
-        <RouterProvider router={router} />
-      </Suspense>
-    </QueryClientProvider>
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />
+    </Suspense>
   );
 }
 
