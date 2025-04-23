34a25b8fbeb55daf7262058af62a8773da5e7387
holajm <holajjm@naver.com>
2025-04-23 18:33:30 +0900

    Feat: 데이터 페칭 로직 axios + RQ 적용

--- src/pages/Market/MarketItemModal.tsx
+++ src/pages/Market/MarketItemModal.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 
 import { useCoinState, useModalState, useUserStore } from "@store/store";
+import useCustomAxios from "@hooks/useCustomAxios";
 import useImage from "@hooks/useImage";
 import Button from "@components/Button";
 
 import style from "@styles/Market/MarketItemModal.module.css";
+import { useQuery } from "@tanstack/react-query";
 
-interface ModalData {
-  grade: string;
-  imageUrl: string;
-  name: string;
-  stock: number;
-  price: number;
-}
+// interface ModalData {
+//   grade: string;
+//   imageUrl: string;
+//   name: string;
+//   stock: number;
+//   price: number;
+// }
 
 function MarketItemModal({
   clickItemId,
@@ -24,36 +26,51 @@ function MarketItemModal({
   onKeyPress: (e: React.KeyboardEvent<HTMLDivElement>) => void;
 }) {
   const SERVER_API = import.meta.env.VITE_SERVER_API;
-  const [modalData, setModalData] = useState<ModalData>({
-    grade: "",
-    imageUrl: "",
-    name: "",
-    stock: 0,
-    price: 0,
-  });
+  const axios = useCustomAxios();
+  // const [modalData, setModalData] = useState<ModalData>({
+  //   grade: "",
+  //   imageUrl: "",
+  //   name: "",
+  //   stock: 0,
+  //   price: 0,
+  // });
   const { user } = useUserStore((state) => state);
   const { modalClose } = useModalState((state) => state);
   const divRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    divRef.current?.focus()
-  },[])
-  const getModalData = async () => {
-    const response = await fetch(
-      `${SERVER_API}/items/${clickItemId}/products`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${user?.accessToken}`,
-        },
-      },
-    );
-    const data = await response.json();
-    setModalData(data?.data);
-  };
-  useEffect(() => {
-    getModalData();
+    divRef.current?.focus();
   }, []);
+  const getModalData = async () => {
+    const response = await axios.get(
+      `${SERVER_API}/items/${clickItemId}/products`,
+    );
+    return response;
+  };
+  const { data } = useQuery({
+    queryKey: ["MarketModal", clickItemId, user],
+    queryFn: getModalData,
+    throwOnError: true,
+    select: (data) => data?.data,
+  });
+  console.log(data);
+
+  // const getModalData = async () => {
+  //   const response = await fetch(
+  //     `${SERVER_API}/items/${clickItemId}/products`,
+  //     {
+  //       method: "GET",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${user?.accessToken}`,
+  //       },
+  //     },
+  //   );
+  //   const data = await response.json();
+  //   setModalData(data?.data);
+  // };
+  // useEffect(() => {
+  //   getModalData();
+  // }, []);
 
   // 재고가 없을 경우 구매 불가 예외 처리 로직 구현 요망
   const coinRefresh = useCoinState((state) => state.coinRefresh);
@@ -76,7 +93,12 @@ function MarketItemModal({
     }
   };
   return (
-    <div onKeyDown={onKeyPress} tabIndex={0} ref={divRef} className={style.container}>
+    <div
+      onKeyDown={onKeyPress}
+      tabIndex={0}
+      ref={divRef}
+      className={style.container}
+    >
       <main className={style.wrapper}>
         <p className={style.background}></p>
         <button onClick={modalClose} className={style.close_button}>
@@ -84,26 +106,26 @@ function MarketItemModal({
         </button>
         <section className={style.section}>
           <aside className={style.section_aside}>
-            <img src={useImage(modalData?.imageUrl)} alt="sample" />
+            <img src={useImage(data?.imageUrl)} alt="sample" />
           </aside>
           <article className={style.section_article}>
             <aside className={style.section_article_aside}>
               <p>
-                이름 <span>{modalData?.name}</span>
+                이름 <span>{data?.name}</span>
               </p>
               <p>
-                등급 <span>{modalData?.grade}</span>
+                등급 <span>{data?.grade}</span>
               </p>
               <p>
-                가격 <span>{modalData?.price}</span>
+                가격 <span>{data?.price}</span>
               </p>
               <p>
-                수량 <span>{modalData?.stock}</span>
+                수량 <span>{data?.stock}</span>
               </p>
             </aside>
             {/* 구매 기능 구현 및 공용 버튼 컴포넌트로 변경 */}
             <div className={style.section_article_button}>
-              {modalData?.stock === 0 ? null : (
+              {data?.stock === 0 ? null : (
                 <Button text={"구매"} width={"5rem"} onClick={buyItem}></Button>
               )}
             </div>
