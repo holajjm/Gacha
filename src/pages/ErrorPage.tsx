89ad1adc9237b25076fd85b49f0bcdf0ede30dad
holajm <holajjm@naver.com>
2025-04-28 17:46:50 +0900

    Feat: 에러 페이지 추가

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+
+import Footer from "@components/layouts/Footer";
+import Header from "@components/layouts/Header";
+import style from "@styles/ErrorPage.module.css";
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <>
+      <Header />
+      <section className={style.section}>
+        <article className={style.wrapper}>
+          <h1 className={style.error_title}>
+            Page not Found
+          </h1>
+          <p className={style.error_desc}>
+            서버와의 통신이 원활하지 않아
+            <br />
+            데이터를 불러올 수 없습니다
+          </p>
+          <img className={style.image} src="/images/Error.webp" alt="error" width={480} height={480} />
+        </article>
+      </section>
+      <Footer />
+    </>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;
