82fe42310868250c85b3cfeeb86ecd52e24cdcf7
holajm <holajjm@naver.com>
2025-05-02 16:04:43 +0900

    Refactor: 알림 데이터 타입 수정 및 출력 문구 수정

--- src/components/BellItems.tsx
+++ src/components/BellItems.tsx
@@ -1,57 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 import Lotto from "./Lotto";
+import BellItem from "./BellItem";
 import style from "@styles/Layouts/BellItems.module.css";
 
-interface NotiDetailData {
-  itemGrade?: string;
-  itemName?: string;
-  coin?: number;
-}
 interface Notis {
   id: number;
-  type: string;
-  data: NotiDetailData;
-  read: boolean;
+  data: string;
+  notificationType: string; //lotto_issued trade_completed
 }
 interface NotiData {
   count: number;
-  hasNewNotifications: boolean;
   notifications: Notis[];
 }
 
-function BellItem({
-  data,
-  handleLotto,
-}: {
-  data: NotiDetailData;
-  handleLotto: () => void;
-}) {
-  return (
-    <article className={style.article}>
-      {data?.itemGrade ? (
-        <p onClick={handleLotto}>
-          {data?.itemGrade}등급의 아이템을 모두 모아 복권이 지급되었습니다.
-          여기를 눌러 복권 결과를 확인해보세요!
-        </p>
-      ) : (
-        <p>
-          마켓에 등록한 {data?.itemName}가 판매되어 {data?.coin} 코인이
-          지급되었습니다.
-        </p>
-      )}
-    </article>
-  );
-}
-
 function BellItems({ data }: { data: NotiData }) {
-  const [notis, setNotis] = useState<Notis[]>([]);
-  useEffect(() => {
-    setNotis(data?.notifications);
-  }, [data]);
-  const itemList = notis.map((e) => (
-    <BellItem key={e?.id} data={e?.data} handleLotto={handleLotto} />
-  ));
+  console.log(data);
 
   const [lotto, setLotto] = useState(false);
   console.log(lotto);
@@ -67,6 +31,9 @@ function BellItems({ data }: { data: NotiData }) {
       setLotto(false);
     }
   };
+  const itemList = data?.notifications.map((e) => (
+    <BellItem key={e?.id} data={e} handleLotto={handleLotto} />
+  ));
   return (
     <>
       {lotto ? (
@@ -78,13 +45,6 @@ function BellItems({ data }: { data: NotiData }) {
         ) : (
           <span className={style.noItem}>알림이 없습니다.</span>
         )}
-        {/* <p className={style.article}>
-          마켓에 등록한 아이템이 판매되어 100 코인이 지급되었습니다.
-        </p>
-        <p className={style.article} onClick={handleLotto}>
-          A등급의 아이템을 모두 모아 복권이 지급되었습니다. 여기를 눌러 복권
-          결과를 확인해보세요!
-        </p> */}
       </section>
     </>
   );
