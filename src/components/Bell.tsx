1cb2e1abed16f26081d6ebe58660f23ffda29d98
holajm <holajjm@naver.com>
2025-05-02 16:03:59 +0900

    Feat: 알림 호출 로직 axios + RQ 전환 및 토스트 알림 설정

--- src/components/Bell.tsx
+++ src/components/Bell.tsx
@@ -1,107 +1,63 @@
 import React, { useEffect, useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { useQueries } from "@tanstack/react-query";
 
 import { useUserStore } from "@store/store";
 import useCustomAxios from "@hooks/useCustomAxios";
+import { toast } from "react-toastify";
 
 import BellItems from "./BellItems";
 import style from "@styles/Layouts/Bell.module.css";
 
-interface NotiDetailData {
-  itemGrade?: string;
-  itemName?: string;
-  coin?: number;
-}
-interface NotiData {
-  count: number;
-  hasNewNotifications: boolean;
-  notifications: {
-    id: number;
-    type: string;
-    data: NotiDetailData;
-    read: boolean;
-  }[];
-}
 function Bell() {
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
   const axios = useCustomAxios();
+
   const [click, setClick] = useState(false);
-  const [noti, setNoti] = useState<NotiData>({
-    count: 0,
-    hasNewNotifications: false,
-    notifications: [
-      {
-        id: 0,
-        type: "LOTTO_ISSUED",
-        data: {
-          itemGrade: "",
-          itemName: "",
-          coin: 0,
-        },
-        read: false,
-      },
-    ],
-  });
+  const openBell = () => {
+    setClick(!click);
+  };
+
   const getNoti = async () => {
     const response = await axios.get(`${SERVER_API}/notifications`);
     return response;
   };
-  const { data } = useQuery({
-    queryKey: ["Noti",user],
-    queryFn: getNoti,
-    select: (data) => data?.data,
+  const getNewNoti = async () => {
+    const response = await axios.get(`${SERVER_API}/notifications/has_new`);
+    return response;
+  };
+
+  const Notis = useQueries({
+    queries: [
+      { queryKey: ["Noti", user], queryFn: getNoti },
+      { queryKey: ["isNewNoti", user], queryFn: getNewNoti },
+    ],
   });
-  console.log(data);
+  const notis = Notis[0]?.data?.data;
+  const isNewNotis = Notis[1]?.data?.data;
 
+  // console.log("notis", notis);
+  // console.log("isNewNotis", isNewNotis);
   useEffect(() => {
-    setNoti(data);
-  }, [data]);
-
-  const readNoti = async () => {
-    try {
-      if (noti?.count) {
-        const response = await fetch(`${SERVER_API}/notifications/mark`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${user?.accessToken}`,
-          },
-          body: JSON.stringify({
-            notificationIds: noti?.notifications[0]?.id,
-          }),
-        });
-        const data = await response.json();
-        if (data?.result === "SUCCESS") {
-          console.log(data);
-        } else if (data?.result === "ERROR") {
-          console.log(data?.error.message);
-          alert(data?.error.message);
-        }
-      }
-      return;
-    } catch (error) {
-      console.log(error);
+    if (isNewNotis?.hasNewNotifications) {
+      toast("새로운 알림이 있습니다.");
     }
-  };
-  const openBell = () => {
-    setClick(!click);
-  };
-  // console.log(noti);
+  }, []);
 
   return (
     <>
       <aside
         onClick={() => {
-          readNoti();
           openBell();
         }}
         className={style.container}
       >
         <img src="/images/Bell.webp" alt="Bell" />
-        {data?.count ? <span className={style.isItem}></span> : null}
+        {isNewNotis?.hasNewNotifications ? (
+          <span className={style.isItem}></span>
+        ) : null}
       </aside>
-      {click ? <BellItems data={noti} /> : null}
+      {click ? <BellItems data={notis} /> : null}
     </>
   );
 }
