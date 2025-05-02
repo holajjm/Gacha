10492f5c29c47c10138707cbba5fe483b0f793aa
holajm <holajjm@naver.com>
2025-05-02 16:02:26 +0900

    Feat: SSE 연결 메인페이지에서 전역으로 변경 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 
+import useSSE from "@hooks/useSSE";
 import Loading from "@components/Loading";
+import { toast, ToastContainer } from "react-toastify";
 
 import "./App.css";
 
 function App() {
+  const { SSECompleted, SSETestData, SSETest2Data } = useSSE();
+  console.log("SSECompleted", SSECompleted);
+  console.log("SSETestData", SSETestData);
+  console.log("SSETest2Data", SSETest2Data);
+
+  useEffect(() => {
+    if (SSETestData?.data) {
+      toast(`${SSETestData?.data}`);
+    }
+  }, [SSETestData]);
+  useEffect(() => {
+    if (SSETest2Data?.data) {
+      toast(`${SSETest2Data?.data}`);
+    }
+  }, [SSETest2Data]);
   return (
     <Suspense fallback={<Loading />}>
+      <ToastContainer
+        position="top-right"
+        limit={2}
+        closeButton={true}
+        autoClose={3000}
+      />
       <RouterProvider router={router} />
     </Suspense>
   );
