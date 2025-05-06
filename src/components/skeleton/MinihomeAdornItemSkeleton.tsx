18326be3c48c95cd8eb3eb48abe8271b85cd739d
holajm <holajjm@naver.com>
2025-05-06 14:15:42 +0900

    Feat: 꾸미기 아이템 목록 스켈레톤 UI 생성

---
+++ src/components/skeleton/MinihomeAdornItemSkeleton.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import style from "@styles/Layouts/skeleton/MinihomeAdornItemSkeleton.module.css";
+
+function MinihomeAdornItemSkeleton() {
+  return (
+    <div className={style.wrapper}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default MinihomeAdornItemSkeleton;
