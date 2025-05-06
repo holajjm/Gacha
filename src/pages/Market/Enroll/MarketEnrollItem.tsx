d867e82f4ff203974c1a5fe439b1322cc3f68bb5
holajm <holajjm@naver.com>
2025-05-06 14:50:54 +0900

    Refactor: 마켓 판매 가능 아이템 목록 내 이미지 최적화 적용

--- src/pages/Market/Enroll/MarketEnrollItem.tsx
+++ src/pages/Market/Enroll/MarketEnrollItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import useImage from "@hooks/useImage";
 
@@ -22,16 +22,28 @@ function MarketEnrollItem({
   item: Item;
   onSelect: (item: Item) => void;
 }) {
+  const imgRef = useRef<HTMLImageElement>(null);
+  useEffect(() => {
+    if (imgRef.current) {
+      imgRef.current.setAttribute("fetchpriority", "high");
+    }
+  }, []);
   const handleClick = () => {
     onSelect(item);
   };
 
   return (
-    <article className={style[`article_${item?.itemGrade}`]} onClick={handleClick}>
+    <article
+      className={style[`article_${item?.itemGrade}`]}
+      onClick={handleClick}
+    >
       <img
         className={style.article_img}
         src={useImage(item?.imageUrl)}
         alt="sample"
+        ref={imgRef}
+        width={96}
+        height={96}
       />
       {item?.itemCnt > 0 ? (
         <p className={style.article_cnt}>{item?.itemCnt}</p>
