f71972d917be056e7503262d638cf44ecbf92317
holajm <holajjm@naver.com>
2025-05-06 14:18:13 +0900

    Feat: 아이템 목록 7개 단위 추가 호출 로직 구현

--- src/pages/Minihome/Adorn/MinihomeAdornItemList.tsx
+++ src/pages/Minihome/Adorn/MinihomeAdornItemList.tsx
@@ -4,6 +4,7 @@ import { useUserStore } from "@store/store";
 
 import MinihomeAdornItem from "./MinihomeAdornItem";
 import style from "@styles/Minihome/Adorn/MinihomeAdornItemList.module.css";
+import MinihomeAdornItemSkeleton from "@components/skeleton/MinihomeAdornItemSkeleton";
 
 interface AdornItemData {
   imageUrl: string;
@@ -20,27 +21,85 @@ function MinihomeAdornItemList({
   const SERVER_API = import.meta.env.VITE_SERVER_API;
   const { user } = useUserStore((state) => state);
   const [itemList, setItemList] = useState<AdornItemData[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const [pageParams, setPageParams] = useState<boolean>(false);
+
   const getItems = async () => {
-    const response = await fetch(`${SERVER_API}/items/${user?.nickname}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user?.accessToken}`,
+    if (pageParams) return;
+    const response = await fetch(
+      `${SERVER_API}/items/${user?.nickname}?page=${page}&size=7&sort=createdAt,desc`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.accessToken}`,
+        },
       },
-    });
+    );
     const data = await response.json();
-    setItemList(data?.data?.content);
+    // console.log(data?.data);
+    // console.log(data?.data?.content);
+    if (data?.data?.content) {
+      setItemList((prev) => [...prev, ...data.data.content]);
+    }
+    if (data?.data?.last) {
+      setPageParams(data?.data?.last);
+    }
   };
   useEffect(() => {
     getItems();
-  }, []);
+  }, [page]);
+  // console.log("page", page);
+  // console.log("pageParams", pageParams);
+  // console.log("itemList", itemList);
+
   const handleClickItem = (data: AdornItemData) => {
     getItem(data);
   };
   const items = itemList.map((e) => (
-    <MinihomeAdornItem key={e.itemId} data={e} onClick={handleClickItem} />
+    <MinihomeAdornItem key={e.subId} data={e} onClick={handleClickItem} />
   ));
-  return <section className={style.main}>{items}</section>;
+
+  // const obsRef = useRef<HTMLParagraphElement | null>(null);
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((entry) => {
+  //     const first = entry[0];
+  //     if (first.isIntersecting && !pageParams) {
+  //       console.log("🔽 Intersecting. Loading next page...");
+  //       setPage((prev) => prev + 1);
+  //     } else {
+  //       console.log("False");
+  //     }
+  //   });
+  //   const target = obsRef.current;
+  //   if (target) {
+  //     observer.observe(target);
+  //   }
+  //   return () => {
+  //     if (target) {
+  //       observer.unobserve(target);
+  //     }
+  //   };
+  // }, [pageParams]);
+  const moreData = () => {
+    if (pageParams) return;
+    setPage(page + 1);
+  };
+  return (
+    <>
+      {itemList.length ? (
+        <section className={style.main}>
+          {items}
+          {/* <p ref={obsRef}>Load More</p> */}
+          {!pageParams ? <button className={style.loadBtn} onClick={moreData}>
+            더보기...
+          </button> : null}
+        </section>
+      ) : (
+        <MinihomeAdornItemSkeleton />
+      )}
+    </>
+  );
 }
 
 export default MinihomeAdornItemList;
