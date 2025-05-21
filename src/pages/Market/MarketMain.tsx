8ef962ff202d5405227a424d196555081b43eb8d
holajm <holajjm@naver.com>
2025-05-21 22:22:02 +0900

    Feat: 모달창 포탈 적용

--- src/pages/Market/MarketMain.tsx
+++ src/pages/Market/MarketMain.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import useCustomAxios from "@hooks/useCustomAxios";
 import { useModalState, useUserStore } from "@store/store.ts";
 import Button from "@components/Button";
+import { ModalPortal } from "@hooks/ModalPortal";
 import usePageTitle from "@hooks/usePageTitle";
 import usePageUpper from "@hooks/usePageUpper";
 
@@ -69,95 +70,109 @@ function MarketMain() {
   ));
 
   return (
-    <div className={style.container}>
-      <main className={style.wrapper}>
-        <header className={style.header}>
-          <aside className={style.header_aside}>
-            <Button
-              text={<SlArrowLeft />}
-              width={"2.5rem"}
-              onClick={() => window.history.back()}
-            />
-            <h1 className={style.header_title}>마켓</h1>
-          </aside>
-          <nav className={style.header_nav}>
-            <Button
-              text={"내 판매 목록"}
-              width={"10rem"}
-              onClick={() => navigate("/market/mysellingitem")}
-            ></Button>
-            <Button
-              text={"내 상품 등록"}
-              width={"10rem"}
-              onClick={() => navigate("/market/enroll")}
-            ></Button>
-          </nav>
-        </header>
-        <section className={style.section}>
-          <nav onClick={handleClick} className={style.section_nav}>
-            <button
-              datatype=""
-              className={navClick === "" ? style.active_button : style.button}
-            >
-              All
-            </button>
-            <button
-              datatype="S"
-              className={navClick === "S" ? style.active_button : style.button}
-            >
-              S등급
-            </button>
-            <button
-              datatype="A"
-              className={navClick === "A" ? style.active_button : style.button}
-            >
-              A등급
-            </button>
-            <button
-              datatype="B"
-              className={navClick === "B" ? style.active_button : style.button}
-            >
-              B등급
-            </button>
-            <button
-              datatype="C"
-              className={navClick === "C" ? style.active_button : style.button}
-            >
-              C등급
-            </button>
-            <button
-              datatype="D"
-              className={navClick === "D" ? style.active_button : style.button}
-            >
-              D등급
-            </button>
-          </nav>
-          <article className={style.article}>
-            {isLoading ? (
-              <div className={style.article_loading}>
-                {/* <img
+    <>
+      {modal && (
+        <ModalPortal>
+          <MarketItemModal
+            clickItemId={clickItemId}
+            onKeyPress={handleKeyPress}
+          />
+        </ModalPortal>
+      )}
+      <div className={style.container}>
+        <main className={style.wrapper}>
+          <header className={style.header}>
+            <aside className={style.header_aside}>
+              <Button
+                text={<SlArrowLeft />}
+                width={"2.5rem"}
+                onClick={() => window.history.back()}
+              />
+              <h1 className={style.header_title}>마켓</h1>
+            </aside>
+            <nav className={style.header_nav}>
+              <Button
+                text={"내 판매 목록"}
+                width={"10rem"}
+                onClick={() => navigate("/market/mysellingitem")}
+              ></Button>
+              <Button
+                text={"내 상품 등록"}
+                width={"10rem"}
+                onClick={() => navigate("/market/enroll")}
+              ></Button>
+            </nav>
+          </header>
+          <section className={style.section}>
+            <nav onClick={handleClick} className={style.section_nav}>
+              <button
+                datatype=""
+                className={navClick === "" ? style.active_button : style.button}
+              >
+                All
+              </button>
+              <button
+                datatype="S"
+                className={
+                  navClick === "S" ? style.active_button : style.button
+                }
+              >
+                S등급
+              </button>
+              <button
+                datatype="A"
+                className={
+                  navClick === "A" ? style.active_button : style.button
+                }
+              >
+                A등급
+              </button>
+              <button
+                datatype="B"
+                className={
+                  navClick === "B" ? style.active_button : style.button
+                }
+              >
+                B등급
+              </button>
+              <button
+                datatype="C"
+                className={
+                  navClick === "C" ? style.active_button : style.button
+                }
+              >
+                C등급
+              </button>
+              <button
+                datatype="D"
+                className={
+                  navClick === "D" ? style.active_button : style.button
+                }
+              >
+                D등급
+              </button>
+            </nav>
+            <article className={style.article}>
+              {isLoading ? (
+                <div className={style.article_loading}>
+                  {/* <img
                   src="/images/Loading.webp"
                   alt="Loading"
                   width={256}
                   height={256}
                 /> */}
-                <MinihomeItemSkeleton />
-              </div>
-            ) : (
-              <section className={style.article_section}>
-                {renderItemList}
-              </section>
-            )}
-          </article>
-        </section>
-      </main>
-      {modal ? (
-        <MarketItemModal
-          clickItemId={clickItemId}
-          onKeyPress={handleKeyPress}
-        />
-      ) : null}
-    </div>
+                  <MinihomeItemSkeleton />
+                </div>
+              ) : (
+                <section className={style.article_section}>
+                  {renderItemList}
+                </section>
+              )}
+            </article>
+          </section>
+        </main>
+      </div>
+    </>
   );
 }
 
