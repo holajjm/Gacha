fa4e7f49bba5187e74a4ca841b50a9b54a3ed66a
holajm <holajjm@naver.com>
2025-05-21 14:14:24 +0900

    Feat: useMutation 마이그레이션, 캐싱 무효화 + 자동 재요청 로직 추가

--- src/pages/Market/MarketItemModal.tsx
+++ src/pages/Market/MarketItemModal.tsx
@@ -1,49 +1,31 @@
 import React, { useEffect, useRef } from "react";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
-import { useCoinState, useMarketModalState, useUserStore } from "@store/store.ts";
+import { useModalState, useUserStore } from "@store/store.ts";
 import useCustomAxios from "@hooks/useCustomAxios";
 import useImage from "@hooks/useImage";
 import Button from "@components/Button";
+import { toast } from "react-toastify";
 
 import style from "@styles/Market/MarketItemModal.module.css";
-import { useQuery } from "@tanstack/react-query";
-
-// interface ModalData {
-//   grade: string;
-//   imageUrl: string;
-//   name: string;
-//   stock: number;
-//   price: number;
-// }
 
 function MarketItemModal({
   clickItemId,
-  // onClick,
   onKeyPress,
 }: {
   clickItemId: number;
-  // onClick: () => void;
   onKeyPress: (e: React.KeyboardEvent<HTMLDivElement>) => void;
 }) {
-  const SERVER_API = import.meta.env.VITE_SERVER_API;
   const axios = useCustomAxios();
-  // const [modalData, setModalData] = useState<ModalData>({
-  //   grade: "",
-  //   imageUrl: "",
-  //   name: "",
-  //   stock: 0,
-  //   price: 0,
-  // });
+  const queryClient = useQueryClient();
   const { user } = useUserStore((state) => state);
-  const { modalClose } = useMarketModalState((state) => state);
+  const { modalClose } = useModalState((state) => state);
   const divRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     divRef.current?.focus();
   }, []);
   const getModalData = async () => {
-    const response = await axios.get(
-      `${SERVER_API}/items/${clickItemId}/products`,
-    );
+    const response = await axios.get(`/items/${clickItemId}/products`);
     return response;
   };
   const { data } = useQuery({
@@ -54,44 +36,33 @@ function MarketItemModal({
   });
   console.log(data);
 
-  // const getModalData = async () => {
-  //   const response = await fetch(
-  //     `${SERVER_API}/items/${clickItemId}/products`,
-  //     {
-  //       method: "GET",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${user?.accessToken}`,
-  //       },
-  //     },
-  //   );
-  //   const data = await response.json();
-  //   setModalData(data?.data);
-  // };
-  // useEffect(() => {
-  //   getModalData();
-  // }, []);
-
-  // 재고가 없을 경우 구매 불가 예외 처리 로직 구현 요망
-  const coinRefresh = useCoinState((state) => state.coinRefresh);
-  const buyItem = async () => {
-    if (confirm("구매하시겠습니까?")) {
-      try {
-        await fetch(`${SERVER_API}/items/${clickItemId}/purchase`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${user?.accessToken}`,
-          },
-        });
-        alert("구매 성공");
-        coinRefresh();
-        window.location.reload();
-      } catch (error) {
-        console.error(error);
+  const { mutate: buyItem } = useMutation({
+    mutationFn: async () => {
+      if (confirm(`${data?.name}을 구매하시겠습니까?`)) {
+        try {
+          const response = await axios.post(
+            `/items/${clickItemId}/purchase`,
+            {},
+          );
+          toast("구매 성공!");
+          return response?.data;
+        } catch (error) {
+          console.log(error);
+        }
       }
-    }
-  };
+    },
+    onSuccess: (data) => {
+      if (data?.error) {
+        console.log(data?.error?.message);
+      }
+      queryClient.invalidateQueries({ queryKey: ["coin"] });
+      queryClient.invalidateQueries({ queryKey: ["MarketItem"] });
+    },
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
   return (
     <div
       onKeyDown={onKeyPress}
@@ -123,7 +94,6 @@ function MarketItemModal({
                 수량 <span>{data?.stock}</span>
               </p>
             </aside>
-            {/* 구매 기능 구현 및 공용 버튼 컴포넌트로 변경 */}
             <div className={style.section_article_button}>
               {data?.stock === 0 ? null : (
                 <Button text={"구매"} width={"5rem"} onClick={buyItem}></Button>
