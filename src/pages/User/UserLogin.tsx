f5db921d48391157b9f9b18024f68371a7e11cae
holajm <holajjm@naver.com>
2025-05-22 16:53:09 +0900

    Refactor: 시멘틱 태그 적용

--- src/pages/User/UserLogin.tsx
+++ src/pages/User/UserLogin.tsx
@@ -6,7 +6,7 @@ import usePageUpper from "@hooks/usePageUpper";
 import style from "@styles/User/UserLogin.module.css";
 
 function UserLogin() {
-  usePageTitle("Login");
+  usePageTitle("로그인");
   usePageUpper();
   const CLIENT_ID = import.meta.env.VITE_KAKAO_CLIENT_ID;
   const REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
@@ -25,33 +25,41 @@ function UserLogin() {
 
   return (
     <div className={style.container}>
-      <section className={style.wrapper}>
+      <section className={style.wrapper} aria-labelledby="login-title">
         <header className={style.header}>
-          <h1 className={style.title}>
+          <h1 id="login-title" className={style.title}>
             Welcome to the
             <br />
             GACHAGACHA!
           </h1>
         </header>
         <main className={style.main}>
-          <div className={style.main_wrapper}>
-            <button onClick={kakaoLogin} className={style.kakao}>
-              <img
-                src="/images/TemporaryKakao.webp"
-                alt="KakaoLogin"
-                width={24}
-                height={24}
-              />
-              <p>카카오로 시작하기</p>
-            </button>
-            <button onClick={githubLogin} className={style.github}>
-              <img
-                src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
-                alt="GitHub Logo"
-              />
-              <p>깃허브로 시작하기</p>
-            </button>
-          </div>
+          <article className={style.main_wrapper}>
+            <nav aria-label="Social login">
+              <ul className={style.login_list}>
+                <li>
+                  <button onClick={kakaoLogin} className={style.kakao}>
+                    <img
+                      src="/images/TemporaryKakao.webp"
+                      alt="카카오 로고"
+                      width={24}
+                      height={24}
+                    />
+                    <span>카카오로 시작하기</span>
+                  </button>
+                </li>
+                <li>
+                  <button onClick={githubLogin} className={style.github}>
+                    <img
+                      src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                      alt="GitHub 로고"
+                    />
+                    <span>깃허브로 시작하기</span>
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </article>
         </main>
       </section>
     </div>
