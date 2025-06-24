9e35517a22af50fc3694ab6e5c263ec37360078b
holajm <holajjm@naver.com>
2025-06-24 17:07:10 +0900

    Refactor: 불필요 중복 태그 제거

--- src/pages/Market/Enroll/MarketEnrollPreview.tsx
+++ src/pages/Market/Enroll/MarketEnrollPreview.tsx
@@ -62,43 +62,41 @@ function MarketEnrollPreview({ item }: { item: Item }) {
 
   return (
     <section className={style.section}>
-      <span className={style.header_background}></span>
+      <span className={style.background}></span>
       {item?.imageUrl ? (
         <>
           <header className={style.section_header}>{imageTag}</header>
           <article className={style.section_article}>
-            <span className={style.section_article_wrapper}>
-              <table>
-                <thead>
-                  <tr>
-                    <th>이름</th>
-                    <th>등급</th>
-                    <th>가격</th>
-                    <th>마켓 재고</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{item?.itemName}</td>
-                    <td>{item?.itemGrade}</td>
-                    <td>{item?.price}코인</td>
-                    <td>{item?.stock}개</td>
-                  </tr>
-                </tbody>
-              </table>
-              <span className={style.section_article_button}>
-                <Button
-                  text={"판매 등록"}
-                  width={"6rem"}
-                  onClick={enrollItem}
-                ></Button>
-              </span>
+            <table>
+              <thead>
+                <tr>
+                  <th>이름</th>
+                  <th>등급</th>
+                  <th>가격</th>
+                  <th>재고</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{item?.itemName}</td>
+                  <td>{item?.itemGrade}</td>
+                  <td>{item?.price}코인</td>
+                  <td>{item?.stock}개</td>
+                </tr>
+              </tbody>
+            </table>
+            <span className={style.section_article_button}>
+              <Button
+                text={"판매 등록"}
+                width={"6rem"}
+                onClick={enrollItem}
+              ></Button>
             </span>
           </article>
         </>
       ) : (
-        <div className={style.section_header_wrapper}>
-          <p className={style.section_header_text}>
+        <div className={style.section_no_contents}>
+          <p className={style.section_no_text}>
             판매하실 아이템을 선택해주세요.
           </p>
         </div>
