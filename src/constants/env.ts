36f6619ef3cf681858c6709b4abb0d9139023eeb
holajm <holajjm@naver.com>
2025-07-29 13:23:58 +0900

    Feat: 환경변수 파일 생성

---
+++ src/constants/env.ts
@@ -0,0 +1,7 @@
+export const ENV = {
+  //KAKAO API
+  KAKAO_CLIENT_ID: import.meta.env.VITE_KAKAO_CLIENT_ID,
+  KAKAO_REDIRECT_URI: import.meta.env.VITE_KAKAO_REDIRECT_URI,
+  //SERVER
+  SERVER_API: import.meta.env.VITE_SERVER_API,
+};
