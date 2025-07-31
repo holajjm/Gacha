fd68087e63cd61a44d3f1a647d25744b0b688b52
holajm <holajjm@naver.com>
2025-07-31 14:19:51 +0900

    Refactor: 이미지 최적화 속성 수정

--- src/pages/Minihome/ItemBook/MinihomeItems.tsx
+++ src/pages/Minihome/ItemBook/MinihomeItems.tsx
@@ -1,18 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 import useImage from "@hooks/useImage";
-
 import style from "@styles/Minihome/ItemBook/MinihomeItem.module.css";
-import { ItemBookData } from "types/minihome";
 
+import type { ItemBookData } from "types/minihome";
 
 function MinihomeItems({ data }: { data: ItemBookData }) {
-  const imgRef = useRef<HTMLImageElement>(null);
-  useEffect(() => {
-    if (imgRef.current) {
-      imgRef.current.setAttribute("fetchpriority", "high");
-    }
-  }, []);
   return (
     <article className={style[`article_item_${data?.itemGrade}`]}>
       {!data?.itemCnt ? <p className={style.article_item_disabled}></p> : null}
@@ -20,9 +13,10 @@ function MinihomeItems({ data }: { data: ItemBookData }) {
         <img
           src={useImage(data?.imageUrl)}
           alt="sample"
-          ref={imgRef}
           width={96}
           height={96}
+          {...{ fetchpriority: "high" }}
+          decoding="async"
         />
         {data?.itemCnt ? (
           <p className={style.article_item_count}>{data?.itemCnt}개</p>
