b773f3488d641448d5d52d2ffd71cd810f5a996c
holajm <holajjm@naver.com>
2025-07-31 16:40:46 +0900

    Fix: 스켈레톤 UI 레이아웃 변경

--- src/components/skeleton/MinihomeItemSkeleton.tsx
+++ src/components/skeleton/MinihomeItemSkeleton.tsx
@@ -4,7 +4,7 @@ import style from "@styles/Layouts/skeleton/MinihomeItemSkeleton.module.css";
 
 function MinihomeItemSkeleton() {
   return (
-    <div className={style.wrapper}>
+    <>
       <div className={style.item}>
         <div></div>
       </div>
@@ -83,7 +83,7 @@ function MinihomeItemSkeleton() {
       <div className={style.item}>
         <div></div>
       </div>
-    </div>
+    </>
   );
 }
 
