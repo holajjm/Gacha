37597be1ad7e0b92c6bc1eb74d6a00ee082d6dc1
holajm <holajjm@naver.com>
2025-07-31 16:55:07 +0900

    Refactor: 가챠 캡슐 컴포넌트 로직 정리

--- src/pages/Gacha/GachaCapsule.tsx
+++ src/pages/Gacha/GachaCapsule.tsx
@@ -1,24 +1,18 @@
 import React, { useEffect, useRef, useState } from "react";
 
-import GachaOpenCapsule from "./GachaOpenCapsule";
-import GachaCloseCapsule from "./GachaCloseCapsule";
 import style from "@styles/Gacha/GachaCapsule.module.css";
 
-function GachaCapsule({ itemImageUrl }: { itemImageUrl: string }) {
-  const [color, setColor] = useState<string>("");
-  const [randomNum, setRandomNum] = useState<number>(0);
-  const colorArr = ["Blue", "Pink", "Yellow", "Green"];
+import GachaOpenCapsule from "./GachaOpenCapsule";
+import GachaCloseCapsule from "./GachaCloseCapsule";
 
+function GachaCapsule({ itemImageUrl }: { itemImageUrl: string }) {
+  const [randomNum, setRandomNum] = useState<number>(0);
   useEffect(() => {
     setRandomNum(Math.floor(Math.random() * 4));
   }, []);
 
-  useEffect(() => {
-    setColor(colorArr[randomNum]);
-  }, [randomNum]);
-
   const timerRef = useRef<number>(0);
-  const [showComponent, setShowComponent] = useState("none");
+  const [showComponent, setShowComponent] = useState<string>("none");
   const openModal = () => {
     setShowComponent("Close");
     timerRef.current = setTimeout(() => {
@@ -33,10 +27,16 @@ function GachaCapsule({ itemImageUrl }: { itemImageUrl: string }) {
     <div className={style.container}>
       <p className={style.background}></p>
       <main className={style.wrapper}>
-        {showComponent === "Open" && (
-          <GachaOpenCapsule color={color} itemImageUrl={itemImageUrl} />
+        {showComponent === "Close" ? (
+          <GachaCloseCapsule
+            color={["Blue", "Pink", "Yellow", "Green"][randomNum]}
+          />
+        ) : (
+          <GachaOpenCapsule
+            color={["Blue", "Pink", "Yellow", "Green"][randomNum]}
+            itemImageUrl={itemImageUrl}
+          />
         )}
-        {showComponent === "Close" && <GachaCloseCapsule color={color} />}
       </main>
     </div>
   );
