b014fdeb0798b2adb4bf0bbcebd3ca679e7db056
holajm <holajjm@naver.com>
2025-07-31 17:03:56 +0900

    Feat: 판매 등록 API 로직 분리

--- src/pages/Market/Enroll/MarketEnrollPreview.tsx
+++ src/pages/Market/Enroll/MarketEnrollPreview.tsx
@@ -1,47 +1,14 @@
 import React from "react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import useCustomAxios from "@hooks/useCustomAxios";
-import useImage from "@hooks/useImage";
 import Button from "@components/Button";
-import { toast } from "react-toastify";
-
+import { useEnrollQuery } from "@features/market/useEnrollQuery";
+import useImage from "@hooks/useImage";
 import style from "@styles/Market/Enroll/MarketEnrollPreview.module.css";
-import { Item } from "types/market";
 
+import type { Item } from "types/market";
 
 function MarketEnrollPreview({ item }: { item: Item }) {
-  const axios = useCustomAxios();
-  const queryClient = useQueryClient();
-  const { mutate: enrollItem } = useMutation({
-    mutationFn: async () => {
-      if (confirm("상품을 판매하시겠습니까?")) {
-        try {
-          const response = await axios.post("/products", {
-            userItemId: item?.userItemIds[0],
-          });
-          return response?.data;
-        } catch (error) {
-          console.log(error);
-        }
-      }
-    },
-    onSuccess: (data) => {
-      if (data?.error) {
-        console.log(data?.error?.message);
-        alert(data?.error?.message);
-        return;
-      }
-      queryClient.invalidateQueries({ queryKey: ["EnrollItemList"] });
-      queryClient.invalidateQueries({ queryKey: ["SellingItems"] });
-      queryClient.invalidateQueries({ queryKey: ["MarketItem"] });
-      toast("상품이 등록되었습니다!");
-      console.log(data);
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
+  const { mutate: enrollItem } = useEnrollQuery({ item });
 
   const imageTag = (
     <img
